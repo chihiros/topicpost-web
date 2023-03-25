4c2c701293fedfa6de79a63bb0417a96feabe3e6
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 00:08:19 +0900

    fix: ファイル名変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Top from './apps/components/templates/Sidebar';
+import Top from './apps/components/templates/Top';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
