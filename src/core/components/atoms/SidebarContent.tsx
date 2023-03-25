18312e36969dce7fe21a37081f0ae5d67cec2569
chihiros <szrkw2ui2a@gmail.com>
2023-03-25 22:39:42 +0900

    fix: textを太字に修正

--- src/core/components/atoms/SidebarContent.tsx
+++ src/core/components/atoms/SidebarContent.tsx
@@ -11,7 +11,7 @@ const SidebarContent: React.FC<Props> = ({ title }) => {
         <div className="bg-zinc-200 rounded-md">
           <svg aria-hidden="true" className="w-12 h-12" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
         </div>
-        <span className="ml-4 text-lg">{title}</span>
+        <span className="ml-4 text-lg font-semibold">{title}</span>
       </a>
     </li>
   );
