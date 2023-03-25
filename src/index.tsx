b72f04baf94f06c141626723c58765410b7c2b5a
chihiros <szrkw2ui2a@gmail.com>
2023-03-25 22:43:43 +0900

    fix: templateのファイル名変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Top from './apps/components/templates/Top';
+import Top from './apps/components/templates/Sidebar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
