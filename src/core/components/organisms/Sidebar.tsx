74b4e7458c663ad39c5368657fc3d716bd82d8a5
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 00:10:23 +0900

    del: 不要なファイルの削除

--- src/core/components/organisms/Sidebar.tsx
+++ src/core/components/organisms/Sidebar.tsx
@@ -1,10 +1,23 @@
-import SidebarMenu from "../molecules/Sidebar/SidebarMenu";
+import SiteLogo from "../molecules/Sidebar/SidebarLogo";
+import Login from "../molecules/Sidebar/SidebarLogin";
+import SidebarContent from "../molecules/Sidebar/SidebarLabel";
 import SidebarFooter from "../molecules/Sidebar/SidebarFooter";
 
 const Sidebar = () => {
   return (
     <aside id="logo-sidebar" className="fixed top-0 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 shadow" aria-label="Sidebar">
-      <SidebarMenu />
+      <div className="h-full py-4 overflow-y-auto bg-gray-50">
+        <SiteLogo />
+        <Login />
+        <ul className="">
+          <SidebarContent
+            label="レクリエーション"
+          />
+          <SidebarContent
+            label="活動日記"
+          />
+        </ul>
+      </div>
       <SidebarFooter />
     </aside>
   );
