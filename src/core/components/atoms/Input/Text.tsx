70599c1583ea208111fa6f7b113b41c35995189e
chihiros <szrkw2ui2a@gmail.com>
2023-03-27 00:07:23 +0900

    add: Textのcomponent追加

---
+++ src/core/components/atoms/Input/Text.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type TextProps = {
+  id: string;
+  type: string;
+  placeholder: string;
+  required?: boolean;
+};
+
+const Text: React.FC<TextProps> = ({ id, type, placeholder, required }) => {
+  return (
+    <input
+      type={type}
+      id={id}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      placeholder={placeholder}
+      {...(required ? { required } : {})}
+    />
+  );
+};
+
+export default Text;
