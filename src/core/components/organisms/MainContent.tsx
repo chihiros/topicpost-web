a0006b98b21947beeaaeedf5558d3a47b33cb300
chihiros <szrkw2ui2a@gmail.com>
2023-03-27 03:57:27 +0900

    del: paddingの削除

--- src/core/components/organisms/MainContent.tsx
+++ src/core/components/organisms/MainContent.tsx
@@ -6,7 +6,7 @@ const MainContent = () => {
 
       <Header />
 
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+      <div className="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
         <div className="grid grid-cols-3 gap-4 mb-4">
           <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
             <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
