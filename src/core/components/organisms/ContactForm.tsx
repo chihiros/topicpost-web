08b36cc24cd43e2b6efe09ab324076bc0a4d2c4e
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 23:27:57 +0900

    del: 不要なHTMLを削除

--- src/core/components/organisms/ContactForm.tsx
+++ src/core/components/organisms/ContactForm.tsx
@@ -30,12 +30,6 @@ const ContactForm: React.FC = () => {
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">連絡先</label>
             <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="contact@example.com" required />
           </div>
-          <div className="flex items-start mb-6">
-            <div className="flex items-center h-5">
-              <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
-            </div>
-            <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900">I agree with the <a href="/contact" className="text-blue-600 hover:underline">terms and conditions</a></label>
-          </div>
           <div className="mb-6">
             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
             <textarea id="message" rows={4} className="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..."></textarea>
