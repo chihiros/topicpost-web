1759ab26ed2e441fbf1089f98d00390c059fa29f
chihiros <szrkw2ui2a@gmail.com>
2023-03-27 03:56:23 +0900

    add: Headerの追加

--- src/core/components/organisms/MainContent.tsx
+++ src/core/components/organisms/MainContent.tsx
@@ -1,6 +1,11 @@
+import Header from "../molecules/Header/Header";
+
 const MainContent = () => {
   return (
     <div className="p-4 sm:ml-64">
+
+      <Header />
+
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
         <div className="grid grid-cols-3 gap-4 mb-4">
           <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
