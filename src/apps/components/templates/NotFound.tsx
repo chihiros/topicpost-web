9986b259579ec68e8ba9c2b7b44a0518869553e3
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 14:05:11 +0900

    add: Sidebarの追加

--- src/apps/components/templates/NotFound.tsx
+++ src/apps/components/templates/NotFound.tsx
@@ -1,6 +1,9 @@
+import Sidebar from "../../../core/components/organisms/Sidebar";
+
 const NotFound: React.FC = () => {
   return (
     <div>
+      <Sidebar />
       <h1>404 Not Found</h1>
     </div>
   );
