a15e910b414978b16436528475cffde2bdcc204d
chihiros <szrkw2ui2a@gmail.com>
2023-03-27 00:07:52 +0900

    add: input type=textのcomponentを追加

---
+++ src/core/components/molecules/Input/InputText.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Label from "../../atoms/Label";
+import Text from "../../atoms/Input/Text";
+
+type InputTextProps = {
+  id: string;
+  type: string;
+  label: string;
+  placeholder: string;
+  required?: boolean;
+};
+
+const InputText: React.FC<InputTextProps> = ({ id, type, label, placeholder, required }) => {
+  return (
+    <div>
+      <Label htmlFor={id}>{label}</Label>
+      <Text
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
+};
+export default InputText;
