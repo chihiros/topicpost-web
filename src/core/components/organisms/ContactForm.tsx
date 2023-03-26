b45f9e41c43eeb20d305ab9a9fce22ed3ebfbabe
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 23:32:11 +0900

    fix: ボタンの中身を変更

--- src/core/components/organisms/ContactForm.tsx
+++ src/core/components/organisms/ContactForm.tsx
@@ -34,7 +34,7 @@ const ContactForm: React.FC = () => {
             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">お問い合わせ内容</label>
             <textarea id="message" rows={4} className="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Register new account</button>
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">送信</button>
         </form>
       </div>
     </div>
