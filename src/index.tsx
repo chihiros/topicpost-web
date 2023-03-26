7f7a76e263d5b8fb701a8cb730daa125b5c99dd4
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 13:30:07 +0900

    del: 一時削除

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import Top from './apps/components/templates/Top';
 import Recreation from './apps/components/templates/Recreation';
 import Diary from './apps/components/templates/Diary';
 import Contact from './apps/components/templates/Contact';
-import Login from './apps/components/templates/Login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
@@ -19,7 +18,6 @@ root.render(
         <Route path="/recreation" component={Recreation} />
         <Route path="/diary" component={Diary} />
         <Route path="/contact" component={Contact} />
-        <Route path="/login" component={Login} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
