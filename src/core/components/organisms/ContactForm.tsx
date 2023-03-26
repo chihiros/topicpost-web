56b759e7d89b0aaf5215b2ef40fe56a60c544078
chihiros <szrkw2ui2a@gmail.com>
2023-03-27 00:28:12 +0900

    add: InputTextareaを使った形に書き換え

--- src/core/components/organisms/ContactForm.tsx
+++ src/core/components/organisms/ContactForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import InputText from '../molecules/Input/InputText';
+import InputTextarea from '../molecules/Input/InputTextarea';
 
 const ContactForm: React.FC = () => {
   return (
@@ -41,9 +42,12 @@ const ContactForm: React.FC = () => {
               placeholder="contact@example.com"
               required={true} />
           </div>
-          <div className="mb-6">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">お問い合わせ内容</label>
-            <textarea id="message" rows={4} className="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Leave a comment..." required></textarea>
+          <div className="mb-3">
+            <InputTextarea
+              id="message"
+              label="お問い合わせ内容"
+              placeholder="お問い合わせ内容を入力してください。"
+              required={true} />
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">送信</button>
         </form>
