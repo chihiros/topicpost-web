638f8a7ee0cfe4ec007a91d9678ebd93f3353b67
chihiros <szrkw2ui2a@gmail.com>
2023-03-26 23:08:15 +0900

    fix: componentの書き方修正

--- src/apps/components/templates/Contact.tsx
+++ src/apps/components/templates/Contact.tsx
@@ -1,7 +1,7 @@
 import Sidebar from "../../../core/components/organisms/Sidebar";
 import ContactForm from "../../../core/components/organisms/ContactForm";
 
-const Contact = () => {
+const Contact: React.FC = () => {
   return (
     <div>
       <Sidebar />
