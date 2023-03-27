6a36247a66265b390640e0dc622e8532e3d91ac4
chihiros <szrkw2ui2a@gmail.com>
2023-03-28 00:26:59 +0900

    del: 使っていないやつは削除

--- src/core/components/organisms/RecreationContent.tsx
+++ src/core/components/organisms/RecreationContent.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Header from '../molecules/Header/Header';
 
 const ReacreationForm: React.FC = () => {
