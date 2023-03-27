64ae3912325a939f3bff6ade3feeb3379fa05f7d
chihiros <szrkw2ui2a@gmail.com>
2023-03-28 00:59:56 +0900

    add: ArrowのIconを追加

--- src/core/components/molecules/Header/Header.tsx
+++ src/core/components/molecules/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ArrowRight from '../../atoms/Icon/Arrow';
 
 type BreadcrumbProps = {
   href: string;
@@ -21,7 +22,7 @@ const Header: React.FC<{ breadcrumb?: BreadcrumbProps[] }> = ({ breadcrumb }) =>
             return (
               <li>
                 <div className="flex items-center">
-                  <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                <ArrowRight />
                   <a href={item.href} className="ml-1 text-sm font-medium md:ml-2 hover:text-blue-600">{item.label}</a>
                 </div>
               </li>
@@ -30,7 +31,7 @@ const Header: React.FC<{ breadcrumb?: BreadcrumbProps[] }> = ({ breadcrumb }) =>
             return (
               <li aria-current="page">
                 <div className="flex items-center">
-                  <svg aria-hidden="true" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                  <ArrowRight />
                   <span className="ml-1 text-sm font-medium md:ml-2">{item.label}</span>
                 </div>
               </li>
