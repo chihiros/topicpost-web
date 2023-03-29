9bc7ef2d39ab9e5a11f2c59be7fb2be5990fb2ee
chihiros <szrkw2ui2a@gmail.com>
2023-03-30 03:30:19 +0900

    fix: テンプレートの修正

--- src/apps/components/templates/ContactTemplate.tsx
+++ src/apps/components/templates/ContactTemplate.tsx
@@ -1,13 +1,7 @@
-import SidebarPage from "../pages/SidebarPage";
 import ContactForm from "../../../core/components/organisms/ContactForm";
 
 const Contact: React.FC = () => {
-  return (
-    <div>
-      <SidebarPage />
-      <ContactForm />
-    </div>
-  );
+  return <ContactForm />;
 }
 
 export default Contact;
