3ec8ddff082467182b66c4f36db782b5fd051a0b
chihiros <szrkw2ui2a@gmail.com>
2023-03-30 03:30:08 +0900

    fix: ぱんくずりの削除

--- src/core/components/organisms/ContactForm.tsx
+++ src/core/components/organisms/ContactForm.tsx
@@ -42,51 +42,43 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <div className="p-4 sm:ml-64">
-      <Breadcrumb
-        breadcrumb={[
-          { href: '/contact', context: 'お問い合わせ' },
-        ]}
-      />
-
-      <div className="p-4 shadow-md bg-gray-50 rounded-lg">
-        <div className="flex mb-5 text-3xl">お問い合わせ</div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-6">
-            <InputText
-              id="name"
-              type="text"
-              label="お名前"
-              placeholder="テスト太郎"
-              required={true}
-              value={nameValue}
-              onChange={handleTextChange}
-            />
-          </div>
-          <div className="mb-6">
-            <InputText
-              id="email"
-              type="email"
-              label="連絡先"
-              placeholder="contact@example.com"
-              required={true}
-              value={emailValue}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="mb-3">
-            <InputTextarea
-              id="message"
-              label="お問い合わせ内容"
-              placeholder="お問い合わせ内容を入力してください。"
-              required={true}
-              value={messageValue}
-              onChange={handleMessageChange}
-            />
-          </div>
-          <SubmitButton>送信</SubmitButton>
-        </form>
-      </div>
+    <div className="p-4 shadow-md bg-gray-50 rounded-lg">
+      <div className="flex mb-5 text-3xl">お問い合わせ</div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <InputText
+            id="name"
+            type="text"
+            label="お名前"
+            placeholder="テスト太郎"
+            required={true}
+            value={nameValue}
+            onChange={handleTextChange}
+          />
+        </div>
+        <div className="mb-6">
+          <InputText
+            id="email"
+            type="email"
+            label="連絡先"
+            placeholder="contact@example.com"
+            required={true}
+            value={emailValue}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div className="mb-3">
+          <InputTextarea
+            id="message"
+            label="お問い合わせ内容"
+            placeholder="お問い合わせ内容を入力してください。"
+            required={true}
+            value={messageValue}
+            onChange={handleMessageChange}
+          />
+        </div>
+        <SubmitButton>送信</SubmitButton>
+      </form>
     </div>
   );
 }
