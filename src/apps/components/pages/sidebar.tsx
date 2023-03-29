de3871456d33152b63dd23f2c79c5ad1e385e438
chihiros <szrkw2ui2a@gmail.com>
2023-03-30 01:35:11 +0900

    add: sidebarのデータを追加

---
+++ src/apps/components/pages/sidebar.tsx
@@ -0,0 +1,25 @@
+export type SidebarProps = {
+  icon : string;
+  label : string;
+  link : string;
+};
+
+export const sidebar: SidebarProps[] = [
+  {
+    icon: "🔔",
+    label: "お知らせ",
+    link: "/news"
+  }, {
+    icon: "🏠",
+    label: "レクリエーション",
+    link: "/recreation"
+  }, {
+    icon: "📚",
+    label: "活動日記",
+    link: "/diary"
+  }, {
+    icon: "📝",
+    label: "お問い合わせ",
+    link: "/contact"
+  }
+];
