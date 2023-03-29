4175b4a77c7b4c735d1547769140220068bed471
chihiros <szrkw2ui2a@gmail.com>
2023-03-30 01:45:04 +0900

    add: Sidebarの内容をPagesに移動

--- src/apps/components/pages/SidebarPage.tsx
+++ src/apps/components/pages/SidebarPage.tsx
@@ -1,8 +1,10 @@
-import SidebarLogin from "./Sidebar/SidebarLogin";
-import SidebarLabel from "./Sidebar/SidebarLabel";
-import SidebarFooter from "./Sidebar/SidebarFooter";
+import React from 'react';
+import SidebarLogin from '../../../core/components/organisms/Sidebar/SidebarLogin';
+import SidebarLabel from '../../../core/components/organisms/Sidebar/SidebarLabel';
+import SidebarFooter from '../../../core/components/organisms/Sidebar/SidebarFooter';
+import { sidebar } from './sidebar';
 
-const Sidebar: React.FC = () => {
+const SidebarPage: React.FC = () => {
   return (
     <aside id="logo-sidebar" className="fixed top-0 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 shadow" aria-label="Sidebar">
       <div className="h-full py-4 overflow-y-auto bg-gray-50">
@@ -10,7 +12,7 @@ const Sidebar: React.FC = () => {
           <span className="text-3xl font-semibold hover:text-gray-400">TopicPost</span>
         </a>
         <SidebarLogin />
-        {labels.map((label, index) => (
+        {sidebar.map((label, index) => (
           <SidebarLabel
             key={index}
             icon={label.icon}
@@ -24,4 +26,4 @@ const Sidebar: React.FC = () => {
   );
 }
 
-export default Sidebar;
+export default SidebarPage;
