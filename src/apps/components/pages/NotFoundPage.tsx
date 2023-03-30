c55c42a1704eddb99eb7bcf73dcb363f6e7d818c
chihiros <szrkw2ui2a@gmail.com>
2023-03-31 03:10:51 +0900

    add: テストでModalの追加

--- src/apps/components/pages/NotFoundPage.tsx
+++ src/apps/components/pages/NotFoundPage.tsx
@@ -1,24 +1,87 @@
 import React from "react";
-import SidebarPage from "./SidebarPage";
 
 const NotFoundImage = `${process.env.PUBLIC_URL}/images/not_found_404.png`;
 
 const NotFound: React.FC = () => {
+  const [showLoginModal, setShowLoginModal] = React.useState(false);
+
+  const handleModalClose = () => {
+    setShowLoginModal(false);
+  };
+
   return (
-    <div>
-      <SidebarPage />
-      <div className="sm:ml-64">
-        <div className="flex mt-20 mb-6 justify-center">
-          <img className="" src={NotFoundImage} alt="" />
-        </div>
-        <div className="flex mb-6 justify-center text-3xl font-semibold text-gray-500">
-          Oops, this page doesn't seem to exist.
-        </div>
-        <div className="flex justify-center text-lg text-gray-500">
-          <a href="/" className="hover:text-blue-500">Go to the top page.</a>
-        </div>
+    <>
+      <div className="flex mt-20 mb-6 justify-center">
+        <img className="" src={NotFoundImage} alt="" />
       </div>
-    </div>
+      <div className="flex mb-6 justify-center text-3xl font-semibold text-gray-500">
+        Oops, this page doesn't seem to exist.
+      </div>
+      <div className="flex justify-center text-lg text-gray-500">
+        <a href="/" className="hover:text-blue-500">Go to the top page.</a>
+      </div>
+
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowLoginModal(true)}
+      >
+        Open regular modal
+      </button>
+
+      {showLoginModal && (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={handleModalClose}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={handleModalClose}
+                  >
+                    <span className="text-black h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleModalClose}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowLoginModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
+    </>
   );
 }
 
