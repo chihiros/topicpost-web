7127546fd6fa41f8f5c4dc31dad6fd8d2d3cd39e
chihiros <szrkw2ui2a@gmail.com>
2023-03-31 20:40:04 +0900

    add: LoginModalの追加

---
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+export interface LoginModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (email: string, password: string) => void;
+  modalOpen: () => void;
+}
+
+const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(email, password);
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={onClose}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Login</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={onClose}
+                  >
+                    <span className="text-black h-6 w-6 text-2xl block outline-none focus:outline-none">×</span>
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                      <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                        Email
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+                        Password
+                      </label>
+                      <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="******************"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                      />
+                    </div>
+                    <div className="flex items-center justify-end">
+                      <button
+                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="submit"
+                      >
+                        Login
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default LoginModal;
