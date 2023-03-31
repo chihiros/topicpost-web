60893542bc6bdbcfbb1a2da3f49b6b0d80f9a3e7
chihiros <szrkw2ui2a@gmail.com>
2023-04-01 02:05:56 +0900

    add: リンクを設定

--- src/core/components/organisms/Sidebar/SidebarFooter.tsx
+++ src/core/components/organisms/Sidebar/SidebarFooter.tsx
@@ -5,7 +5,7 @@ const SidebarFooter = () => {
   return (
     <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200">
       <span className="flex text-sm text-gray-500 justify-center mb-3">
-        Powered by chihiro.suzurikawa
+        <a href="https://github.com/chihiros" rel="noreferrer" target="_blank">Powered by chihiro.suzurikawa</a>
       </span>
       <div className="flex mt-4 space-x-6 justify-center">
         <Link to="https://github.com/topics/topicpost" rel="noreferrer" target="_blank" className="text-gray-500 hover:text-gray-900">
