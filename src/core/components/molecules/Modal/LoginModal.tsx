fcecbbc222fa6c1598e55dfd5804b166a45eaefb
chihiros <szrkw2ui2a@gmail.com>
2023-03-31 23:56:45 +0900

    fix: モーダルの外をクリックしたときに閉じる用に変更

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -27,7 +27,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) =>
         <>
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            onClick={onClose}
+            onClick={handleOverlayClick}
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
