f14c283b432ca3c162572b06c9c60cfdea9af64e
chihiros <szrkw2ui2a@gmail.com>
2023-04-01 00:11:58 +0900

    fix: 一時リンクを修正

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -54,11 +54,11 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) =>
                           </div>
                           <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                         </div>
-                        <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                        <a href="/" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                       </div>
                       <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                       <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                        Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                        Not registered? <a href="/" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                       </div>
                     </form>
                   </div>
