e223079fe53f9cf631de0aed666ebca83182ad15
chihiros <szrkw2ui2a@gmail.com>
2023-04-01 04:02:14 +0900

    fix: textの位置を修正

--- src/core/components/molecules/Modal/LoginAuthProviderButton.tsx
+++ src/core/components/molecules/Modal/LoginAuthProviderButton.tsx
@@ -11,10 +11,10 @@ export const AuthProviderButton: React.FC<AuthProviderProps> = ({ icon, children
   return (
     <button
       type="button"
-      className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+      className="grid grid-cols-3 items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
     >
-      <span className='mr-3'>{icon}</span>
-      <span>{children}</span>
+      <span className='tile col-span-1 m-auto'>{icon}</span>
+      <span className='tile col-span-2 mr-auto'>{children}</span>
     </button>
   );
 }
