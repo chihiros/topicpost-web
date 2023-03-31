0f7981f262f4290b9138ca30475c4f9a62bcfe66
chihiros <szrkw2ui2a@gmail.com>
2023-04-01 03:17:46 +0900

    add: SSO用のボタン追加

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -33,29 +33,65 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) =>
                 </button>
                 <div className="px-6 py-6">
                   <h3 className="mb-4 text-xl font-medium text-gray-900">Sign in to our platform</h3>
-                  <form className="space-y-6" action="#">
-                    <div>
-                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
+                  <div className="grid grid-cols-2">
+                    {/* 左側にはSSOログインを設定する */}
+                    <div className="flex flex-col items-center justify-center space-y-4 pr-6">
+                      <button
+                        type="button"
+                        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      >
+                        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fillRule="evenodd" d="M3 10a7 7 0 1114 0 7 7 0 01-14 0zm7-5a5 5 0 00-4.9 6h9.8A5 5 0 0010 5z" clipRule="evenodd"></path></svg>
+                        <span>Sign in with Google</span>
+                      </button>
+                      <button
+                        type="button"
+                        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      >
+                        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 3a7 7 0 100 14A7 7 0 0010 3zm0 12a5 5 0 110-10 5 5 0 010 10z" clipRule="evenodd"></path><path fillRule="evenodd" d="M10 5a3 3 0 100 6 3 3 0 000-6zm0 4a1 1 0 110-2 1 1 0 010 2z" clipRule="evenodd"></path></svg>
+                        <span>Sign in with GitHub</span>
+                      </button>
+                      <button
+                        type="button"
+                        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      >
+                        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 3a7 7 0 100 14A7 7 0 0010 3zm0 12a5 5 0 110-10 5 5 0 010 10z" clipRule="evenodd"></path><path fillRule="evenodd" d="M10 5a3 3 0 100 6 3 3 0 000-6zm0 4a1 1 0 110-2 1 1 0 010 2z" clipRule="evenodd"></path></svg>
+                        <span>Sign in with Twitter</span>
+                      </button>
+                      <button
+                        type="button"
+                        className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      >
+                        <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 3a7 7 0 100 14A7 7 0 0010 3zm0 12a5 5 0 110-10 5 5 0 010 10z" clipRule="evenodd"></path><path fillRule="evenodd" d="M10 5a3 3 0 100 6 3 3 0 000-6zm0 4a1 1 0 110-2 1 1 0 010 2z" clipRule="evenodd"></path></svg>
+                        <span>Sign in with Facebook</span>
+                      </button>
                     </div>
-                    <div>
-                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-                    </div>
-                    <div className="flex justify-between">
-                      <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
+                    {/* 右側にはEmail/Passwordのログインを設定する */}
+                    <div className="h-auto max-w-full border-l-2 pl-6">
+                      <form className="space-y-6" action="#">
+                        <div>
+                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
                         </div>
-                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">Remember me</label>
-                      </div>
-                      <a href="/" className="text-sm text-blue-700 hover:underline">Lost Password?</a>
+                        <div>
+                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+                          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                        </div>
+                        <div className="flex justify-between">
+                          <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                              <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
+                            </div>
+                            <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">Remember me</label>
+                          </div>
+                          <a href="/" className="text-sm text-blue-700 hover:underline">Lost Password?</a>
+                        </div>
+                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
+                        <div className="text-sm font-medium text-gray-500300">
+                          Not registered? <a href="/" className="text-blue-700 hover:underline">Create account</a>
+                        </div>
+                      </form>
                     </div>
-                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
-                    <div className="text-sm font-medium text-gray-500300">
-                      Not registered? <a href="/" className="text-blue-700 hover:underline">Create account</a>
-                    </div>
-                  </form>
+                  </div>
                 </div>
               </div>
             </div>
