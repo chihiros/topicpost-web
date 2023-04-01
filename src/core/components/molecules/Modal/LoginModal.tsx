7b583e11349a6f60705eae7ad929e2b584a3f0ca
chihiros <szrkw2ui2a@gmail.com>
2023-04-02 00:46:52 +0900

    fix: placeholderの中身を変更

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -80,7 +80,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) =>
                       <form className="space-y-6" action="#">
                         <div>
                           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">メールアドレス</label>
-                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
+                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@topicpost.net" required />
                         </div>
                         <div>
                           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">パスワード</label>
