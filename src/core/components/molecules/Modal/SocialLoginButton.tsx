6c56720596b2949b10bf8e99eade687da7d14db1
chihiros <szrkw2ui2a@gmail.com>
2023-04-01 23:10:38 +0900

    fix: ボタンの要素を変更

--- src/core/components/molecules/Modal/SocialLoginButton.tsx
+++ src/core/components/molecules/Modal/SocialLoginButton.tsx
@@ -9,27 +9,12 @@ export interface AuthProviderProps {
 
 export const AuthProviderButton: React.FC<AuthProviderProps> = ({ icon, children, color, link}) => {
   return (
-    <button
-      type="button"
-      className={`
-        grid grid-cols-3
-        items-center justify-center
-        w-full
-        px-4 py-2
-        text-sm font-medium text-gray-700
-        bg-blue-50
-        border border-transparent
-        border-gray-800
-        rounded-md
-        hover:bg-blue-700
-        focus:outline-none
-        focus-visible:ring-2
-        focus-visible:ring-offset-2
-        focus-visible:ring-blue-500
-        `}
-        >
-      <span className='tile col-span-1 m-auto'>{icon}</span>
-      <span className='tile col-span-2 mr-auto'>{children}</span>
+    <button className="group w-full h-10 px-4 border-2 border-gray-300 rounded-full transition duration-300
+                       hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
+     <div className="relative flex items-center space-x-4 justify-start">
+        {icon}
+        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">{children}</span>
+      </div>
     </button>
   );
 }
