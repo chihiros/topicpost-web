a6583a8169159c44c58463365a8233e0268be15b
chihiros <szrkw2ui2a@gmail.com>
2023-04-02 00:35:51 +0900

    fix: 日本語に変更

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -79,11 +79,11 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) =>
                     <div className="h-auto max-w-full border-l-2 pl-6">
                       <form className="space-y-6" action="#">
                         <div>
-                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">メールアドレス</label>
                           <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
                         </div>
                         <div>
-                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Your password</label>
+                          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">パスワード</label>
                           <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                         </div>
                         <div className="flex justify-between">
@@ -91,13 +91,13 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onSubmit }) =>
                             <div className="flex items-center h-5">
                               <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
                             </div>
-                            <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">Remember me</label>
+                            <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">パスワードを記憶する</label>
                           </div>
-                          <a href="/" className="text-sm text-blue-700 hover:underline">Lost Password?</a>
                         </div>
-                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
+                           <a href="/" className="text-sm text-blue-700 hover:underline">パスワードを忘れましたか？</a>
+                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">TopicPost にログイン</button>
                         <div className="text-sm font-medium text-gray-500300">
-                          Not registered? <a href="/" className="text-blue-700 hover:underline">Create account</a>
+                        <a href="/" className="text-blue-700 hover:underline">TopicPostのアカウントを作成する</a>
                         </div>
                       </form>
                     </div>
