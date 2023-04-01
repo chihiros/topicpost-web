e9b7ffedac648c6b8d3c3076298968f5b312d09c
chihiros <szrkw2ui2a@gmail.com>
2023-04-02 00:37:19 +0900

    fix: ボタンの要素を整理する

--- src/core/components/molecules/Modal/SocialLoginButton.tsx
+++ src/core/components/molecules/Modal/SocialLoginButton.tsx
@@ -11,8 +11,8 @@ export const SocialLoginButton: React.FC<SocialLoginProps> = ({ icon, children, 
     <button className="group w-full h-10 px-4 border-2 border-gray-300 rounded-full transition duration-300
                        hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
       <div className="relative items-center space-x-4 justify-start grid grid-cols-12">
-        <span className="tile col-start-1 col-end-3 ml-auto">{icon}</span>
-        <span className="tile col-start-3 col-end-12 block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">{children}</span>
+        <span className="tile col-start-1 col-end-4 ml-auto">{icon}</span>
+        <span className="tile col-start-4 col-end-12 block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">{children}</span>
       </div>
     </button>
   );
