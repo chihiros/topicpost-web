0542f28bf283e4ec69c410abfa1ebd5db8bcdf82
chihiros <szrkw2ui2a@gmail.com>
2023-04-01 23:24:20 +0900

    fix: レイアウトが揃う用にするための微修正

--- src/core/components/molecules/Modal/SocialLoginButton.tsx
+++ src/core/components/molecules/Modal/SocialLoginButton.tsx
@@ -11,9 +11,9 @@ export const AuthProviderButton: React.FC<AuthProviderProps> = ({ icon, children
   return (
     <button className="group w-full h-10 px-4 border-2 border-gray-300 rounded-full transition duration-300
                        hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100">
-     <div className="relative flex items-center space-x-4 justify-start">
-        {icon}
-        <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">{children}</span>
+      <div className="relative items-center space-x-4 justify-start grid grid-cols-12">
+        <span className="tile col-start-1 col-end-3 ml-auto">{icon}</span>
+        <span className="tile col-start-3 col-end-12 block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">{children}</span>
       </div>
     </button>
   );
