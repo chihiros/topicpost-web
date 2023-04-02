aa30c644d7a2f966041aca5771211ec1b2d96273
chihiros <szrkw2ui2a@gmail.com>
2023-04-03 01:42:51 +0900

    add: ToastContainerをPagesに記載

--- src/apps/components/pages/Pages.tsx
+++ src/apps/components/pages/Pages.tsx
@@ -3,6 +3,7 @@ import SidebarPage from "./SidebarPage";
 import Breadcrumb, { BreadcrumbProps } from "../../../core/components/molecules/Breadcrumb/Breadcrumb";
 import { LoginModalProvider } from "../../../context/LoginModalContext";
 import LoginModal from "../../../core/components/molecules/Modal/LoginModal";
+import { ToastContainer } from "react-toastify";
 
 export type PagesProps = {
   breadcrumb?: BreadcrumbProps[];
@@ -13,6 +14,7 @@ const Pages: React.FC<PagesProps> = ({ breadcrumb, template }) => {
   return (
     <LoginModalProvider>
       <div>
+        <ToastContainer />
         <SidebarPage />
         <div className="p-4 sm:ml-64">
           <Breadcrumb breadcrumb={breadcrumb} />
