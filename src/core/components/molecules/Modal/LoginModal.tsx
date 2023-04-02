ce5a544128ea49b23bc20ced47a0e9177bec9016
chihiros <szrkw2ui2a@gmail.com>
2023-04-03 02:02:38 +0900

    fix: aタグからLinkに変更

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -143,7 +143,7 @@ const LoginModal: React.FC = () => {
                           </div>
                         </div>
                         <div className="flex justify-end">
-                          <a href="/forget" className="text-sm text-blue-700 hover:underline">パスワードを忘れましたか？</a>
+                          <Link to="/forget" className="text-sm text-blue-700 hover:underline">パスワードを忘れましたか？</Link>
                         </div>
                         <div
                           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
