600c52d12eeb3f4623811308cebaa51ddc714449
chihiros <szrkw2ui2a@gmail.com>
2023-04-02 15:39:22 +0900

    del: not foundのPageコンポーネントからModalに関係するコードを削除

--- src/apps/components/pages/NotFoundPage.tsx
+++ src/apps/components/pages/NotFoundPage.tsx
@@ -1,13 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import LoginModal from "../../../core/components/molecules/Modal/LoginModal";
-import LoginModalContext from "../../../context/LoginModalContext";
 
 const NotFoundImage = `${process.env.PUBLIC_URL}/images/not_found_404.png`;
 
 const NotFound: React.FC = () => {
-  const { toggle } = useContext(LoginModalContext);
-
   return (
     <>
       <div className="flex mt-20 mb-6 justify-center">
@@ -19,16 +15,6 @@ const NotFound: React.FC = () => {
       <div className="flex justify-center text-lg text-gray-500">
         <Link to="/" className="hover:text-blue-500">Go to the top page.</Link>
       </div>
-
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => toggle()}
-      >
-        Open regular modal
-      </button>
-
-      <LoginModal />
     </>
   );
 }
