c1bdd1f2b5293583082ecba3136229112ffb3abb
chihiros <szrkw2ui2a@gmail.com>
2023-04-03 01:10:51 +0900

    fix: inputのコードを整理

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -88,11 +88,29 @@ const LoginModal: React.FC = () => {
                       <form className="space-y-6" action="#">
                         <div>
                           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">メールアドレス</label>
-                          <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="example@topicpost.net" required />
+                          <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            placeholder="example@topicpost.net"
+                            required
+                          />
                         </div>
                         <div>
                           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">パスワード</label>
-                          <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                          <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            placeholder="••••••••"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            required
+                          />
                         </div>
                         <div className="flex justify-between">
                           <div className="flex items-start">
@@ -105,7 +123,12 @@ const LoginModal: React.FC = () => {
                         <div className="flex justify-end">
                           <a href="/" className="text-sm text-blue-700 hover:underline">パスワードを忘れましたか？</a>
                         </div>
-                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">TopicPost にログイン</button>
+                        <div
+                          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                          onClick={handleLoginClick}
+                        >
+                          TopicPost にログイン
+                        </div>
                         <div className="text-sm font-medium text-gray-500300">
                           <Link to="/signup" className="text-blue-700 hover:underline">TopicPostのアカウントを作成する</Link>
                         </div>
