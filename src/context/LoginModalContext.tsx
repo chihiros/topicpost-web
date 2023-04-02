502cf9e6b20ac9b5d658423de1e2d23c4b239bce
chihiros <szrkw2ui2a@gmail.com>
2023-04-02 13:36:22 +0900

    add: LoginModalの状態を管理するコンテキストを追加

---
+++ src/context/LoginModalContext.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from 'react';
+
+interface LoginModalContextType {
+  isOpen:boolean;
+  toggle: () => void;
+}
+
+const LoginModalContext = createContext<LoginModalContextType>({
+  isOpen: false,
+  toggle: () => {},
+});
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const LoginModalProvider = ({ children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <LoginModalContext.Provider value={{ isOpen, toggle }}>
+      {children}
+    </LoginModalContext.Provider>
+  );
+};
+
+export default LoginModalContext;
