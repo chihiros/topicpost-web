c865a66ac0a8b593de3b4519c53d14373eb8039a
chihiros <szrkw2ui2a@gmail.com>
2023-04-03 00:22:45 +0900

    fix: パスワード記憶のボタンが必須になっていたので任意に修正

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -87,7 +87,7 @@ const LoginModal: React.FC = () => {
                         <div className="flex justify-between">
                           <div className="flex items-start">
                             <div className="flex items-center h-5">
-                              <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
+                              <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" />
                             </div>
                             <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">パスワードを記憶する</label>
                           </div>
