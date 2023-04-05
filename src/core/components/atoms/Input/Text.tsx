cd46b17f2f4ce40277c51fc66793dce251d78e2e
chihiros <szrkw2ui2a@gmail.com>
2023-04-06 00:06:19 +0900

    fix: 書き方を戻す

--- src/core/components/atoms/Input/Text.tsx
+++ src/core/components/atoms/Input/Text.tsx
@@ -14,17 +14,7 @@ const Text: React.FC<TextProps> = ({ id, type, placeholder, required, value, onC
     <input
       type={type}
       id={id}
-      className="
-                bg-gray-50
-                  border border-gray-300
-                  text-gray-900 text-sm
-                  rounded-lg
-                  focus:ring-blue-500
-                  focus:border-blue-500
-                  block
-                  w-full
-                  p-2.5
-                "
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
       {...(placeholder ? { placeholder } : {})}
       {...(required ? { required } : {})}
       {...(value ? { value } : {})}
