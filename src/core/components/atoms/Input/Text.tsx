25d0b26c255c18803235c7e4f0ba64b33a78a6c8
chihiros <szrkw2ui2a@gmail.com>
2023-04-06 00:07:09 +0900

    fix: 変数を使った書き方に修正

--- src/core/components/atoms/Input/Text.tsx
+++ src/core/components/atoms/Input/Text.tsx
@@ -10,15 +10,29 @@ type TextProps = {
 };
 
 const Text: React.FC<TextProps> = ({ id, type, placeholder, required, value, onChange }) => {
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const borderColor = isFocused ? 'border-blue-500' : 'border-gray-300';
+
   return (
     <input
       type={type}
       id={id}
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      className={`bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 ${borderColor}`}
       {...(placeholder ? { placeholder } : {})}
       {...(required ? { required } : {})}
       {...(value ? { value } : {})}
       {...(onChange ? { onChange } : {})}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
     />
   );
 };
