0c00898d3762fdc3362c9259cd9d9f6c8578201c
chihiros <szrkw2ui2a@gmail.com>
2023-04-05 23:08:11 +0900

    fix: classNameを見やすく修正

--- src/core/components/molecules/Modal/LoginModal.tsx
+++ src/core/components/molecules/Modal/LoginModal.tsx
@@ -116,7 +116,14 @@ const LoginModal: React.FC = () => {
                             id="email"
                             value={email}
                             onChange={handleEmailChange}
-                            className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            className="border border-gray-300
+                                      text-gray-900 text-sm
+                                      rounded-lg
+                                      focus:ring-blue-500 focus:border-blue-500
+                                      block
+                                      w-full
+                                      p-2.5
+                                      "
                             placeholder="example@topicpost.net"
                             required
                           />
