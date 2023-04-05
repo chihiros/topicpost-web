b4e5b459e0b9735c709a571785afebbbe2355b0b
chihiros <szrkw2ui2a@gmail.com>
2023-04-06 02:08:58 +0900

    fix: styleを外部からしていできるように修正

--- src/core/components/atoms/Input/Text.tsx
+++ src/core/components/atoms/Input/Text.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 type TextProps = {
   id: string;
   type: string;
+  style?: string;
   placeholder?: string;
   required?: boolean;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Text: React.FC<TextProps> = ({ id, type, placeholder, required, value, onChange }) => {
+const Text: React.FC<TextProps> = ({ id, type, style, placeholder, required, value, onChange }) => {
   const [isFocused, setIsFocused] = React.useState(false);
 
   const handleFocus = () => {
@@ -26,7 +27,7 @@ const Text: React.FC<TextProps> = ({ id, type, placeholder, required, value, onC
     <input
       type={type}
       id={id}
-      className={`bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 ${borderColor}`}
+      className={`border text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 ${borderColor} ${style || ''}`}
       {...(placeholder ? { placeholder } : {})}
       {...(required ? { required } : {})}
       {...(value ? { value } : {})}
