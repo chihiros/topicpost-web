76dd77566280413ffb5afb8064a6e9cb946a848a
chihiros <szrkw2ui2a@gmail.com>
2023-04-07 22:59:43 +0900

    add: ButtonのatomsにonClickの追加

--- src/core/components/atoms/Button/SubmitButton.tsx
+++ src/core/components/atoms/Button/SubmitButton.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 type SubmitProps = {
+  onClick?: () => void;
   children: string;
 };
 
-const SubmitButton: React.FC<SubmitProps> = ({ children }) => {
+const SubmitButton: React.FC<SubmitProps> = ({ onClick, children }) => {
   return (
     <button
       type="submit"
+      onClick={onClick ? onClick : () => {}}
       className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
     >
       {children}
