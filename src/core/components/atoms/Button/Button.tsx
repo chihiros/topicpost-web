de729cb729b11859988b2537b86130f0a55fafeb
chihiros <szrkw2ui2a@gmail.com>
2023-04-07 23:18:09 +0900

    del: 不要なコメントを削除

--- src/core/components/atoms/Button/Button.tsx
+++ src/core/components/atoms/Button/Button.tsx
@@ -14,5 +14,3 @@ const Button: React.FC<ButtonProps> = ({ children, className }) => {
 };
 
 export default Button;
-
-// text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5
