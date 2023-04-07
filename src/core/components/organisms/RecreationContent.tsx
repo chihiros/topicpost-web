3cd0af1a5c4f4cd6cd0c25ac64f26f8d1d790aa9
chihiros <szrkw2ui2a@gmail.com>
2023-04-08 03:39:08 +0900

    fix: Recreationのページを更新

--- src/core/components/organisms/RecreationContent.tsx
+++ src/core/components/organisms/RecreationContent.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { NewCard } from "../atoms/Card";
 
 const RecreationForm: React.FC = () => {
   return (
-    <div className=""></div>
+    <div className="text-2xl">
+      <div className='mb-2 ml-2'>
+        新着情報
+      </div>
+      <div className="flex flex-nowrap overflow-x-auto gap-4">
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+      </div>
+
+    </div>
   );
 }
 
