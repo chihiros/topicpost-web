d3afa0a84f90afa5b45b2a6c79978b3edbb294e3
chihiros <szrkw2ui2a@gmail.com>
2023-04-07 23:31:44 +0900

    fix: ロジックを一箇所にまとめる

--- src/core/components/atoms/Button/Button.tsx
+++ src/core/components/atoms/Button/Button.tsx
@@ -2,10 +2,32 @@ import React, { ReactNode } from 'react';
 
 type ButtonProps = {
   children: ReactNode;
-  className?: string;
+  success?: boolean;
+  warn?: boolean;
+  error?: boolean;
+  submit?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, className }) => {
+const Styles = {
+  success: 'bg-green-500 hover:bg-green-600',
+  warn: 'bg-yellow-500 hover:bg-yellow-600',
+  error: 'bg-red-500 hover:bg-red-600',
+  submit: 'text-white bg-blue-700 hover:bg-blue-800',
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  success,
+  warn,
+  error,
+  submit,
+}) => {
+  let className = '';
+  className = success ? Styles.success : className;
+  className = warn ? Styles.warn : className;
+  className = error ? Styles.error : className;
+  className = submit ? Styles.submit : className;
+
   return (
     <button className={`${className} py-2 px-4 rounded`} type="button">
       {children}
--- src/core/components/atoms/Button/index.tsx
+++ src/core/components/atoms/Button/index.tsx
@@ -6,25 +6,17 @@ type ButtonProps = {
 };
 
 export const SuccessButton: React.FC<ButtonProps> = ({ children }) => {
-  return <Button
-           className='bg-green-500 hover:bg-green-600'
-         >{children}</Button>;
+  return <Button success>{children}</Button>;
 };
 
 export const WarnButton: React.FC<ButtonProps> = ({ children }) => {
-  return <Button
-           className='bg-yellow-500 hover:bg-yellow-600'
-         >{children}</Button>;
+  return <Button warn>{children}</Button>;
 };
 
 export const ErrorButton: React.FC<ButtonProps> = ({ children }) => {
-  return <Button
-           className='bg-red-500 hover:bg-red-600'
-         >{children}</Button>;
+  return <Button error>{children}</Button>;
 };
 
 export const SubmitButton: React.FC<ButtonProps> = ({ children }) => {
-  return <Button
-           className='text-white bg-blue-700 hover:bg-blue-800'
-         >{children}</Button>;
+  return <Button submit>{children}</Button>;
 };
