ed81fe6bf150441dd09ce15c1e41a730cfa118e9
chihiros <szrkw2ui2a@gmail.com>
2023-04-08 02:59:58 +0900

    add: エラーメッセージの追加

--- src/utils/ErrorMessage/index.ts
+++ src/utils/ErrorMessage/index.ts
@@ -4,6 +4,7 @@ type ErrorMessageType = {
 
 export const ErrorMessage: ErrorMessageType = {
   "Invalid login credentials": "ログインに失敗しました",
+  "Rate limit exceeded": "しばらく時間をおいて再度お試しください",
 }
 
 export const getErrorMessage = (error: string) => {
