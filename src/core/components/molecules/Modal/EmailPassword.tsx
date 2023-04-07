7c8047308619be9b75160a7615bb5dab6efb3fab
chihiros <szrkw2ui2a@gmail.com>
2023-04-07 23:48:49 +0900

    fix: atomsのボタンを使う用に修正

--- src/core/components/molecules/Modal/EmailPassword.tsx
+++ src/core/components/molecules/Modal/EmailPassword.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Text } from "../../atoms/Input";
+import { SubmitButton } from "../../atoms/Button";
 
 type EmailPasswordProps = {
   email: string;
@@ -54,12 +55,12 @@ export const EmailPassword: React.FC<EmailPasswordProps> = ({ email, password, o
           パスワードを忘れましたか？
         </Link>
       </div>
-      <div
-        className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      <SubmitButton
+        className="w-full"
         onClick={onClick}
       >
         TopicPost にログイン
-      </div>
+      </SubmitButton>
       <div className="text-sm font-medium text-gray-500">
         <Link
           to="/signup"
