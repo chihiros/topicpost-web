0a037319aea2aa8efb4100c7e970ab4305fe554d
chihiros <szrkw2ui2a@gmail.com>
2023-04-07 23:21:38 +0900

    fix: スタイルの修正

--- src/core/components/atoms/Button/index.tsx
+++ src/core/components/atoms/Button/index.tsx
@@ -25,6 +25,6 @@ export const ErrorButton: React.FC<ButtonProps> = ({ children }) => {
 
 export const SubmitButton: React.FC<ButtonProps> = ({ children }) => {
   return <Button
-           className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5'
+           className='text-white bg-blue-700 hover:bg-blue-800'
          >{children}</Button>;
 };
