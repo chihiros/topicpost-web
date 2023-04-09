b1950be6deb03b65ee0153402844bdfd70f00300
chihiros <szrkw2ui2a@gmail.com>
2023-04-10 00:15:20 +0900

    add: ログイン状態を管理するContextを追加

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useCallback, useEffect, useContext } from 'react';
+import { supabaseClient } from '../utils/supabase';
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  isLoggedIn: false,
+  setLoggedIn: () => {},
+});
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const AuthContextProvider: React.FC<Props> = ({ children }) => {
+  const [, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data: { session } } = await supabaseClient.auth.getSession();
+      setLoggedIn(session !== null);
+    };
+    checkSession();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn: false, setLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
