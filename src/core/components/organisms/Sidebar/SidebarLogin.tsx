16def1a8b0f14538b45b3e1b818cfc5718d19e8d
chihiros <szrkw2ui2a@gmail.com>
2023-04-10 00:35:40 +0900

    del: 使っていないimportを整理

--- src/core/components/organisms/Sidebar/SidebarLogin.tsx
+++ src/core/components/organisms/Sidebar/SidebarLogin.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useLoginModal } from "../../../../context/LoginModalContext";
 
 const SidebarLogin: React.FC = () => {
