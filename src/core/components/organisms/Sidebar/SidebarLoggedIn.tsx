ec2f71c3e7fb68275d6e764d9fc2dccdd20e65b4
chihiros <szrkw2ui2a@gmail.com>
2023-04-10 01:57:55 +0900

    del: 不要なクラスを削除

--- src/core/components/organisms/Sidebar/SidebarLoggedIn.tsx
+++ src/core/components/organisms/Sidebar/SidebarLoggedIn.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SidebarLoggedIn: React.FC = () => {
   return (
     <div>
-      <div className="flex items-center justify-center hover:bg-gray-100 rounded-md p-3 mx-3 h-24 py-3">
+      <div className="flex items-center justify-center hover:bg-gray-100 rounded-md p-3 mx-3 h-24">
         <img className="w-12 h-12 mx-auto rounded-full"
           src="https://ca.slack-edge.com/T02DB7DEGV8-U02L7F0LE56-b5f9688cbca2-512"
           alt=""
