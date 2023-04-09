fb7dffa6a037f45873dc75a61b17f75c754715d2
chihiros <szrkw2ui2a@gmail.com>
2023-04-09 15:41:03 +0900

    add: パスワードを表示うするボタンを追加

--- src/core/components/atoms/Input/Text.tsx
+++ src/core/components/atoms/Input/Text.tsx
@@ -12,6 +12,7 @@ type TextProps = {
 
 const Text: React.FC<TextProps> = ({ id, type, className, placeholder, required, value, onChange }) => {
   const [isFocused, setIsFocused] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
 
   const handleFocus = () => {
     setIsFocused(true);
@@ -21,20 +22,39 @@ const Text: React.FC<TextProps> = ({ id, type, className, placeholder, required,
     setIsFocused(false);
   };
 
+  const handleToggleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const inputType = type === 'password' && showPassword ? 'text' : type;
+  const icon = showPassword ? '🙈' : '🙉';
   const borderColor = isFocused ? 'border-blue-500 ring-1 ring-blue-500' : 'border-gray-300';
 
   return (
-    <input
-      type={type}
-      id={id}
-      className={`border text-gray-900 text-sm rounded-lg block w-full p-2.5 ${borderColor} ${className}`}
-      {...(placeholder ? { placeholder } : {})}
-      {...(required ? { required } : {})}
-      {...(value ? { value } : {})}
-      {...(onChange ? { onChange } : {})}
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-    />
+    <div className="relative">
+      <input
+        type={inputType}
+        id={id}
+        className={`border text-gray-900 text-sm rounded-lg block w-full p-2.5 ${borderColor} ${className}`}
+        {...(placeholder ? { placeholder } : {})}
+        {...(required ? { required } : {})}
+        {...(value ? { value } : {})}
+        {...(onChange ? { onChange } : {})}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+      {type === 'password' && (
+        <button
+          type="button"
+          className="absolute right-2 top-2 transform translate-y-1/2 cursor-pointer focus:outline-none"
+          onClick={handleToggleShowPassword}
+        >
+          <span role="img" aria-label="Toggle password visibility">
+            {icon}
+          </span>
+        </button>
+      )}
+    </div>
   );
 };
 
