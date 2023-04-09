83d2b5056a38b7e7b31541468e306262d757061c
chihiros <szrkw2ui2a@gmail.com>
2023-04-10 01:58:09 +0900

    画像を左寄せに修正

--- src/core/components/organisms/Sidebar/SidebarLoggedIn.tsx
+++ src/core/components/organisms/Sidebar/SidebarLoggedIn.tsx
@@ -4,7 +4,7 @@ const SidebarLoggedIn: React.FC = () => {
   return (
     <div>
       <div className="flex items-center justify-center hover:bg-gray-100 rounded-md p-3 mx-3 h-24">
-        <img className="w-12 h-12 mx-auto rounded-full"
+        <img className="w-12 h-12 mr-auto rounded-full"
           src="https://ca.slack-edge.com/T02DB7DEGV8-U02L7F0LE56-b5f9688cbca2-512"
           alt=""
         />
