1313403624c88741ef47cb3c3323b0ebe92d1002
chihiros <szrkw2ui2a@gmail.com>
2023-04-16 00:14:25 +0900

    add: プロフィール編集用のテンプレー追加

---
+++ src/apps/components/templates/ProfileEditTemplate.tsx
@@ -0,0 +1,10 @@
+import React from "react-router-dom";
+import ProfileEditForm from "../../../core/components/organisms/ProfileEditForm";
+
+const ProfileEdit = () => {
+  return (
+    <ProfileEditForm />
+  );
+}
+
+export default ProfileEdit;
