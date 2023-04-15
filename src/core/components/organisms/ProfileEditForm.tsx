7f13e4af5b9d9d8eaf27b360fa7a9d5955a3d4e8
chihiros <szrkw2ui2a@gmail.com>
2023-04-16 00:17:59 +0900

    ページタイトルの変種

--- src/core/components/organisms/ProfileEditForm.tsx
+++ src/core/components/organisms/ProfileEditForm.tsx
@@ -51,7 +51,7 @@ const ProfileEditForm: React.FC = () => {
 
   return (
     <div className="p-4 bg-gray-50 rounded-lg">
-      <div className="flex mb-5 text-3xl">お問い合わせ</div>
+      <div className="flex mb-5 text-3xl">プロフィールの編集</div>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <Label htmlFor="name">
