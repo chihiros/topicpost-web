697b325c5cbc0f37a1abeea9b0280583076ba4f5
chihiros <szrkw2ui2a@gmail.com>
2023-04-16 23:58:48 +0900

    fix: cssの修正

--- src/core/components/organisms/ProfileEditForm.tsx
+++ src/core/components/organisms/ProfileEditForm.tsx
@@ -139,7 +139,10 @@ const ProfileEditForm: React.FC = () => {
         >
           <label
             htmlFor="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            // className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            className={`flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 ${
+              dragOver ? 'bg-opacity-50' : ''
+            }`}
           >
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
               <svg
@@ -164,7 +167,7 @@ const ProfileEditForm: React.FC = () => {
                 SVG, PNG, JPG or GIF (MAX. 800x400px)
               </p>
             </div>
-            <input id="dropzone-file" type="file" accept="image/*" onChange={handleInputChange} />
+            <input id="dropzone-file" type="file" accept="image/*" hidden onChange={handleInputChange} />
           </label>
         </div>
 
