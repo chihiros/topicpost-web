d306a67a29939d423ec8e801084c738eb1f7baf9
chihiros <szrkw2ui2a@gmail.com>
2023-04-16 23:17:52 +0900

    fix: アップロードしたファイルのプレビューをする用に修正

--- src/core/components/organisms/ProfileEditForm.tsx
+++ src/core/components/organisms/ProfileEditForm.tsx
@@ -8,6 +8,7 @@ import { GetSession } from '../../../utils/supabase';
 
 const ProfileEditForm: React.FC = () => {
   const [nicknameValue, setNicknameValue] = useState('');
+  const [image, setImage] = useState<string | null>(null);
 
   const clearForm = () => {
     setNicknameValue('');
@@ -15,6 +16,15 @@ const ProfileEditForm: React.FC = () => {
 
   const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNicknameValue(event.target.value);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const file = event.target.files?.[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file as Blob);
+    reader.onloadend = () => {
+      setImage(reader.result as string);
+    };
   };
 
   const toast = new Toast();
@@ -94,6 +104,7 @@ const ProfileEditForm: React.FC = () => {
         <Label htmlFor="profile-icon">
           アイコン
         </Label>
+        {image && <img src={image} alt="プレビュー画像" />}　
         <div className="flex items-center justify-center w-full">
           <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -101,7 +112,7 @@ const ProfileEditForm: React.FC = () => {
               <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
               <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
             </div>
-            <input id="dropzone-file" type="file" className="hidden" />
+            <input id="dropzone-file" type="file" accept="image/*" onChange={handleInputChange} />
           </label>
         </div>
 
