1b55e3a1313bbda07e48c68a9a45814359429851
chihiros <szrkw2ui2a@gmail.com>
2023-04-18 22:18:50 +0900

    add: api.topicpost.netのapiを操作する用のclassを追加

---
+++ src/api/api.topicpost.net/index.ts
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+interface Props {}
+
+interface State {
+  url: string | undefined;
+}
+
+export class TopicPostAPI extends Component<Props, State>{
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      // 環境変数からurlを取得する
+      url: process.env.TOPICPOST_API_URL, // https://api.topicpost.net/
+    };
+  }
+}
