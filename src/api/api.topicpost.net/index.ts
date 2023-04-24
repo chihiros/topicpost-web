65512b88b1a5dc2a994d4e9d6b3de95cd52f03c6
chihiros <szrkw2ui2a@gmail.com>
2023-04-24 22:54:54 +0900

    fix: URLを作る場所を修正

--- src/api/api.topicpost.net/index.ts
+++ src/api/api.topicpost.net/index.ts
@@ -2,16 +2,19 @@ import { GetSession } from '../../utils/supabase';
 
 export class TopicPostAPI {
   private baseUrl: string | undefined;
+  private uri: string;
+  private url: string;
 
   constructor() {
     // this.baseUrl = process.env.TOPICPOST_API_URL;
     this.baseUrl = 'https://api.topicpost.net/v1';
+    this.uri = '/profile';
+    this.url = `${this.baseUrl}${this.uri}`;
   }
 
   async get<T>(endpoint: string): Promise<{ data: T, status: number }> {
     const session = await GetSession();
-    const url = `${this.baseUrl}${endpoint}`;
-    const response = await fetch(url, {
+    const response = await fetch(this.url, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${session?.access_token}`,
@@ -31,8 +34,7 @@ export class TopicPostAPI {
 
     async post<T>(endpoint: string, body: any): Promise<{ data: T, status: number }> {
     const session = await GetSession();
-    const url = `${this.baseUrl}${endpoint}`;
-    const response = await fetch(url, {
+    const response = await fetch(this.url, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${session?.access_token}`,
@@ -54,8 +56,7 @@ export class TopicPostAPI {
 
   async put<T>(endpoint: string, body: any): Promise<{ data: T, status: number }> {
     const session = await GetSession();
-    const url = `${this.baseUrl}${endpoint}`;
-    const response = await fetch(url, {
+    const response = await fetch(this.url, {
       method: 'PUT',
       headers: {
         'Authorization': `Bearer ${session?.access_token}`,
@@ -77,8 +78,7 @@ export class TopicPostAPI {
 
   async delete<T>(endpoint: string): Promise<{ data: T, status: number }> {
     const session = await GetSession();
-    const url = `${this.baseUrl}${endpoint}`;
-    const response = await fetch(url, {
+    const response = await fetch(this.url, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${session?.access_token}`,
