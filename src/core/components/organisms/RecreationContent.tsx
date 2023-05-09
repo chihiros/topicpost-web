f42c62e9082258d5a6736455775f8f1536b2faf0
chihiros <szrkw2ui2a@gmail.com>
2023-05-10 01:34:32 +0900

    fix: ドロップメニューが開く用に修正

--- src/core/components/organisms/RecreationContent.tsx
+++ src/core/components/organisms/RecreationContent.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NewCard } from "../atoms/Card";
 
 const RecreationForm: React.FC = () => {
+  const [isActionsDropdownOpen, setActionsDropdownOpen] = useState(false);
+  const [isFilterDropdownOpen, setFilterDropdownOpen] = useState(false);
+
   function smoothScroll(element: Element, distance: number, duration: number) {
     const start = element.scrollLeft
     const startTime = performance.now()
@@ -167,13 +170,18 @@ const RecreationForm: React.FC = () => {
                 </button>
 
                 <div className="flex items-center space-x-3 w-full md:w-auto">
-                  <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
+                  <button
+                    id="actionsDropdownButton"
+                    className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                    type="button"
+                    onClick={() => setActionsDropdownOpen(!isActionsDropdownOpen)}
+                  >
                     <svg className="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                       <path clipRule="evenodd" fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
                     </svg>
                     Actions
                   </button>
-                  <div id="actionsDropdown" className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow">
+                  <div id="actionsDropdown" className={`${isActionsDropdownOpen ? '' : 'hidden'} z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow`}>
                     <ul className="py-1 text-sm text-gray-700" aria-labelledby="actionsDropdownButton">
                       <li>
                         <a href="/" className="block py-2 px-4 hover:bg-gray-100">Mass Edit</a>
@@ -183,7 +191,12 @@ const RecreationForm: React.FC = () => {
                       <a href="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Delete all</a>
                     </div>
                   </div>
-                  <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200" type="button">
+                  <button
+                    id="filterDropdownButton"
+                    className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+                    type="button"
+                    onClick={() => setFilterDropdownOpen(!isFilterDropdownOpen)}
+                  >
                     <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="h-4 w-4 mr-2 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
                     </svg>
@@ -192,7 +205,7 @@ const RecreationForm: React.FC = () => {
                       <path clipRule="evenodd" fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
                     </svg>
                   </button>
-                  <div id="filterDropdown" className="z-10 hidden w-48 p-3 bg-white rounded-lg shadow">
+                  <div id="filterDropdown" className={`${isFilterDropdownOpen ? '' : 'hidden'} z-10 w-48 p-3 bg-white rounded-lg shadow`}>
                     <h6 className="mb-3 text-sm font-medium text-gray-900">Choose brand</h6>
                     <ul className="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
                       <li className="flex items-center">
