3d536724cdc5a81d583d522d6deaa6279adc493e
chihiros <szrkw2ui2a@gmail.com>
2023-05-09 22:47:08 +0900

    del: darkモードの設定を削除

--- src/core/components/organisms/RecreationContent.tsx
+++ src/core/components/organisms/RecreationContent.tsx
@@ -122,8 +122,8 @@ const RecreationForm: React.FC = () => {
           </thead>
           <tbody>
             {RecreationList.map((recreation, index) => (
-              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <tr key={index} className="bg-white border-b">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                   {recreation.title}
                 </th>
                 <td className="px-6 py-4">
