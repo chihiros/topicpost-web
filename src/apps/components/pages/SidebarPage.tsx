184af90b250522320712c9a09e83ccc21a72e2c7
chihiros <szrkw2ui2a@gmail.com>
2023-05-14 13:01:52 +0900

    fix: 改行を追加

--- src/apps/components/pages/SidebarPage.tsx
+++ src/apps/components/pages/SidebarPage.tsx
@@ -18,7 +18,11 @@ const SidebarPage: React.FC = () => {
   };
 
   return (
-    <aside id="logo-sidebar" className="fixed top-0 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 shadow" aria-label="Sidebar">
+    <aside
+      id="logo-sidebar"
+      className="fixed top-0 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 shadow"
+      aria-label="Sidebar"
+    >
       <div className="h-full py-4 overflow-y-auto bg-gray-50">
         <Link to="/" className="flex justify-center pt-2 pb-4">
           <span className="text-3xl font-semibold hover:text-gray-400">TopicPost</span>
