b9452fe44769f899cc49489527b1562785a2c017
chihiros <szrkw2ui2a@gmail.com>
2023-05-15 00:45:52 +0900

    fix: classの修正

--- src/core/components/atoms/Select.tsx
+++ src/core/components/atoms/Select.tsx
@@ -11,7 +11,7 @@ export const Select: React.FC<SelectProps> = ({id, className, children }) => {
   return (
     <select
       id={id}
-      className={`block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer ${className}`}
+      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${className}`}
     >
       {children}
     </select>
