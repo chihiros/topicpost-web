0b15e0d84e69ae07546b606ca8fe1de08692d977
chihiros <szrkw2ui2a@gmail.com>
2023-05-14 18:22:34 +0900

    del: Actionsボタンは不要なため削除

--- src/core/components/organisms/RecreationContent.tsx
+++ src/core/components/organisms/RecreationContent.tsx
@@ -157,35 +157,6 @@ const RecreationForm: React.FC = () => {
                 レクを投稿する
               </button>
 
-              {/* Actionsボタン */}
-              <div className="relative inline-block text-left">
-                <button
-                  id="actionsDropdownButton"
-                  className="w-full sm:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
-                  type="button"
-                  onClick={() => setActionsDropdownOpen(!isActionsDropdownOpen)}
-                >
-                  <svg className="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clipRule="evenodd" fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                  </svg>
-                  Actions
-                </button>
-                <div
-                  ref={actionsDropdownRef}
-                  id="actionsDropdown"
-                  className={`${isActionsDropdownOpen ? '' : 'hidden'} absolute z-50 w-44 top-full bg-white rounded divide-y divide-gray-100 shadow`}
-                >
-                  <ul className="py-1 text-sm text-gray-700" aria-labelledby="actionsDropdownButton">
-                    <li>
-                      <a href="/" className="block py-2 px-4 hover:bg-gray-100">Mass Edit</a>
-                    </li>
-                  </ul>
-                  <div className="py-1">
-                    <a href="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Delete all</a>
-                  </div>
-                </div>
-              </div>
-
               {/* Filterボタン */}
               <div className="relative inline-block text-left">
                 <button
