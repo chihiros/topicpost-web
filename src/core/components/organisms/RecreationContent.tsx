6e695e0b55009cc859d54a473f5f68f05737d896
chihiros <szrkw2ui2a@gmail.com>
2023-05-14 14:29:44 +0900

    fix: react-iconを使った書き方に変更

--- src/core/components/organisms/RecreationContent.tsx
+++ src/core/components/organisms/RecreationContent.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { NewCard } from "../atoms/Card";
+import { AiOutlineSearch, AiOutlinePlus } from "react-icons/ai";
+import { BsFilter } from "react-icons/bs";
 
 const RecreationForm: React.FC = () => {
   const [isActionsDropdownOpen, setActionsDropdownOpen] = useState(false);
@@ -133,9 +135,9 @@ const RecreationForm: React.FC = () => {
                   <label htmlFor="simple-search" className="sr-only">Search</label>
                   <div className="relative w-full">
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-                      </svg>
+                      <AiOutlineSearch
+                        className="w-5 h-5 text-gray-500"
+                      />
                     </div>
                     <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2" placeholder="Search" required />
                   </div>
@@ -143,9 +145,9 @@ const RecreationForm: React.FC = () => {
               </div>
               <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                 <button type="button" className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200">
-                  <svg className="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clipRule="evenodd" fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                  </svg>
+                  <AiOutlinePlus
+                    className="w-4 h-4 mr-2"
+                  />
                   Add product
                 </button>
 
@@ -183,9 +185,9 @@ const RecreationForm: React.FC = () => {
                     type="button"
                     onClick={() => setFilterDropdownOpen(!isFilterDropdownOpen)}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="h-4 w-4 mr-2 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
-                    </svg>
+                    <BsFilter
+                      className="w-5 h-5 mr-2"
+                    />
                     Filter
                     <svg className="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                       <path clipRule="evenodd" fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
