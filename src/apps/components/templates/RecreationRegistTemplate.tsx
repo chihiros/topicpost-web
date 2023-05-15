ca09f9353f1055cf0d418569aab6b870b4de4977
chihiros <szrkw2ui2a@gmail.com>
2023-05-16 03:04:27 +0900

    fix: レスポンシブデザインの修正

--- src/apps/components/templates/RecreationRegistTemplate.tsx
+++ src/apps/components/templates/RecreationRegistTemplate.tsx
@@ -50,7 +50,7 @@ export const RecreationRegistTemplate: React.FC = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
       <div className="p-4 bg-gray-50 rounded-lg">
         <div className="flex mb-5 text-3xl">レクリエーションの投稿</div>
         <form onSubmit={handleSubmit}>
