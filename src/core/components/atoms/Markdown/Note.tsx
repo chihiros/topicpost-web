4233c02e415b4ec46a3e7efc54ce39eb238bd036
chihiros <szrkw2ui2a@gmail.com>
2023-05-16 13:40:04 +0900

    fix: classNameが外部から指定出来るように修正

--- src/core/components/atoms/Markdown/Note.tsx
+++ src/core/components/atoms/Markdown/Note.tsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 type NoteProps = {
   type: 'info' | 'warn' | 'alert';
+  className?: string;
   children: React.ReactNode;
 };
 
-export const Note: React.FC<NoteProps> = ({ type, children }) => {
-  let className;
-
+export const Note: React.FC<NoteProps> = ({ type, children, className }) => {
   switch (type) {
     case 'info':
-      className = 'bg-blue-100 text-blue-700';
+      className += ' bg-blue-100 text-blue-700';
       break;
     case 'warn':
-      className = 'bg-yellow-100 text-yellow-700';
+      className += ' bg-yellow-100 text-yellow-700';
       break;
     case 'alert':
-      className = 'bg-red-100 text-red-700';
+      className += ' bg-red-100 text-red-700';
       break;
   }
 
