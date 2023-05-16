b72cc76f28bee798abb6bfd076d2cb6675e6e609
chihiros <szrkw2ui2a@gmail.com>
2023-05-17 01:53:37 +0900

    fix: Selectコンポーネントを修正

--- src/core/components/atoms/Select.tsx
+++ src/core/components/atoms/Select.tsx
@@ -4,13 +4,16 @@ type SelectProps = {
   id: string;
   className?: string;
   children: React.ReactNode;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
-export const Select: React.FC<SelectProps> = ({id, className, children }) => {
+export const Select: React.FC<SelectProps> = ({id, className, children, value, onChange }) => {
   return (
     <select
       id={id}
+      onChange={onChange}
+      value={value}
       className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
     >
       {children}
