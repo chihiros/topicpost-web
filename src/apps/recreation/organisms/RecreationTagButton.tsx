2bd3e807378ec6cd73d527265439249af602c0e0
chihiros <szrkw2ui2a@gmail.com>
2023-05-22 01:17:51 +0900

    add: タグ用のボタンを追加

---
+++ src/apps/recreation/organisms/RecreationTagButton.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const TagButton: React.FC<Props> = ({ children }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <div className="mr-1 mb-1 inline-block">
+      <input
+        type="checkbox"
+        id="check1"
+        hidden
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)} />
+      <label
+        htmlFor="check1"
+        id="button"
+        className={
+          `p-1 rounded-lg text-xs border border-gray-500
+              ${isChecked ?
+            'bg-cyan-400 text-slate-700' : 'bg-slate-200 text-slate-400'
+          }`}
+      >
+        {children || ''}</label>
+    </div>
+  );
+};
