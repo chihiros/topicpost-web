5488ff8a8ba148e6b70bee44f4f61192cefbba52
chihiros <szrkw2ui2a@gmail.com>
2023-05-21 18:39:43 +0900

    fix: 外部からclassを指定出来るように修正

--- src/core/components/atoms/Label.tsx
+++ src/core/components/atoms/Label.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 type LabelProps = {
   htmlFor?: string;
   children: React.ReactNode;
+  className?: string;
   required?: boolean;
 };
 
-const Label: React.FC<LabelProps> = ({ htmlFor, children, required }) => {
+const Label: React.FC<LabelProps> = ({ htmlFor, children, className, required }) => {
   return (
-    <label htmlFor={htmlFor} className="block mb-2 text-sm font-medium text-gray-900">
+    <label htmlFor={htmlFor} className={`block mb-2 text-sm font-medium text-gray-900 ${className}`}>
       {children}{required && <sup className="text-red-500">*必須</sup>}
     </label>
   );
