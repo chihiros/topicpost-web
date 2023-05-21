385b17613bd351a2365b08d62cd41341270b4ea8
chihiros <szrkw2ui2a@gmail.com>
2023-05-22 00:46:20 +0900

    fix: MarkdownPreviewコンポーネントを使った書き方に修正

--- src/apps/recreation/templates/RecreationRegistTemplate.tsx
+++ src/apps/recreation/templates/RecreationRegistTemplate.tsx
@@ -5,10 +5,7 @@ import Label from "../../../core/components/atoms/Label";
 import { Select } from "../../../core/components/atoms/Select";
 import { Text, Textarea } from "../../../core/components/atoms/Input";
 import { SubmitButton } from "../../../core/components/atoms/Button";
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm'
-import rehypeRaw from 'rehype-raw'
-import { Note } from "../../../core/components/atoms/Markdown";
+import { MarkdownPreview } from "../../../core/components/atoms/Markdown";
 import { RiTimerLine } from "react-icons/ri";
 import { BsFillPeopleFill } from "react-icons/bs";
 import getYouTubeID from "get-youtube-id";
@@ -311,46 +308,13 @@ export const RecreationRegistTemplate: React.FC = () => {
           </style>
 
           {/* Title */}
-          <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              h1: ({ node, children, ...props }) => (
-                <h1 className="font-bold mb-2" {...props}>
-                  {children || ''}
-                </h1>
-              ),
-              hr: ({ node, ...props }) => (
-                <hr className="my-2" {...props} />
-              ),
-            }}
-          >
+          <MarkdownPreview>
             {"# " + recTitleValue + "\n"
               + "---\n"}
-          </ReactMarkdown>
+          </MarkdownPreview>
 
           <div className="text-sm">こんな場面で使えるレクです</div>
-          <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              code: ({ node, inline, className, children, ...props }) => {
-                // const match = /language-(\w+)/.exec(className || '')
-                return inline ? (
-                  <code className="bg-gray-200 text-blue-600 px-1 rounded text-sm inline-block" {...props}>
-                    {children}
-                  </code>
-                ) : (
-                  <pre className="bg-gray-800 text-gray-200 p-0 m-0 rounded" {...props}>
-                    {children}
-                  </pre>
-                )
-              },
-              p: ({ node, ...props }) => (
-                <p className="m-0 mb-1" {...props} />
-              ),
-            }}
-          >
+          <MarkdownPreview>
             {[
               isChecked1 ? "`アイスブレイク`" : ``,
               isChecked2 ? "`手遊びレク`" : ``,
@@ -360,7 +324,7 @@ export const RecreationRegistTemplate: React.FC = () => {
               isChecked6 ? "`レクダン`" : ``,
               isChecked7 ? "`その他のレク`" : ``
             ].filter(i => i !== '').join(' ')}
-          </ReactMarkdown>
+          </MarkdownPreview>
 
           {/* 対象人数・対象年齢・所要時間を表示する */}
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 mt-6 mb-4">
@@ -401,88 +365,9 @@ export const RecreationRegistTemplate: React.FC = () => {
           )}
 
           {/* Rule */}
-          <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              h1: ({ node, children, ...props }) => (
-                <h1 className="font-bold mb-2" {...props}>
-                  {children || ''}
-                </h1>
-              ),
-              h2: ({ node, children, ...props }) => (
-                <h2 className="m-0 font-bold mb-2" {...props}>
-                  {children || ''}
-                </h2>
-              ),
-              h3: ({ node, children, ...props }) => (
-                <h3 className="m-0 font-bold mb-2" {...props}>
-                  {children || ''}
-                </h3>
-              ),
-              h4: ({ node, children, ...props }) => (
-                <h4 className="m-0 font-bold mb-2" {...props}>
-                  {children || ''}
-                </h4>
-              ),
-              h5: ({ node, children, ...props }) => (
-                <h5 className="m-0 mb-2" {...props}>
-                  {children || ''}
-                </h5>
-              ),
-              h6: ({ node, children, ...props }) => (
-                <h6 className="m-0 mb-2" {...props}>
-                  {children || ''}
-                </h6>
-              ),
-              p: ({ node, ...props }) => (
-                <p className="m-0 mb-1" {...props} />
-              ),
-              li: ({ node, children, ...props }) => (
-                <li className="m-0 text-base" {...props} >
-                  {children || ''}
-                </li>
-              ),
-              a: ({ node, children, ...props }) => (
-                <a className="text-blue-600 no-underline" {...props} >
-                  {children || ''}
-                </a>
-              ),
-              code: ({ node, inline, className, children, ...props }) => {
-                // const match = /language-(\w+)/.exec(className || '')
-                return inline ? (
-                  <code className="bg-gray-200 text-blue-600 px-1 rounded" {...props}>
-                    {children}
-                  </code>
-                ) : (
-                  <pre className="bg-gray-800 text-gray-200 p-0 m-0 rounded" {...props}>
-                    {children}
-                  </pre>
-                )
-              },
-              pre: ({ node, ...props }) => (
-                <pre className="bg-gray-800 text-gray-200 px-3 mt-3 rounded" {...props} />
-              ),
-              div: ({ node, children }) => {
-                const className = node.properties?.className;
-                if (Array.isArray(className)) {
-                  const stringClassName = className as string[];
-                  if (stringClassName.includes('note')) {
-                    const type = stringClassName.find((cls: string) => ['info', 'warn', 'alert'].includes(cls));
-                    return <Note type={type as 'info' | 'warn' | 'alert'} className="mb-3">{children}</Note>
-                  }
-                }
-                return <div>{children}</div>;
-              },
-              hr: ({ node, ...props }) => (
-                <hr className="my-2" {...props} />
-              ),
-            }}
-          >
-            {messageValue.replace(/:::note (info|warn|alert)\n([\s\S]*?)\n:::/g, (_, type, content) =>
-              `<div class="note ${type}">${content}</div>`
-            )}
-          </ReactMarkdown>
+          <MarkdownPreview>
+            {messageValue}
+          </MarkdownPreview>
         </div>
       </div>
     </div>
