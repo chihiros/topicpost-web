00c5e97ace4b635145cbee0bdc4983ed4ec953ac
chihiros <szrkw2ui2a@gmail.com>
2023-05-26 02:22:04 +0900

    fix: 環境変数からAPIをのPATHを取得するように修正

--- src/api/api.topicpost.net/index.ts
+++ src/api/api.topicpost.net/index.ts
@@ -14,8 +14,8 @@ export class TopicPostAPI {
   private url: string;
 
   constructor(uri: string) {
-    // this.baseUrl = process.env.TOPICPOST_API_URL;
-    this.baseUrl = 'https://api.topicpost.net/v1';
+    this.baseUrl = process.env.TOPICPOST_API_HOST + '/v1';
+    // this.baseUrl = 'https://api.topicpost.net/v1';
     this.url = `${this.baseUrl}${uri}`
   }
 
