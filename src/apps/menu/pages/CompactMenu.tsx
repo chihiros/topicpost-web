6373f925ac6c14d93e9e1236217f11e7f569f132
chihiros <szrkw2ui2a@gmail.com>
2023-05-29 02:42:22 +0900

    fix: document format

--- src/apps/menu/pages/CompactMenu.tsx
+++ src/apps/menu/pages/CompactMenu.tsx
@@ -12,42 +12,42 @@ export const CompactMenu = () => {
 
   return (
     <div className="transition-transform lg:-translate-x-full translate-x-0 lg:hidden">
-    <nav className="bg-gray-50 border-gray-200 rounded-b-3xl">
-      <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-        <a className="" href="/">
-          <span className="text-2xl font-semibold hover:text-gray-400">TopicPost</span>
-        </a>
-        <div className="flex items-center md:order-2">
-          <button
-            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 focus:outline-none"
-            onClick={toggle}
-          >
-            Login
-          </button>
-          <button
-            data-collapse-toggle="mega-menu"
-            type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="mega-menu"
-            aria-expanded="false"
-            onClick={toggleDropdown}
-          >
-            <span className="sr-only">Open main menu</span>
-            <FiMenu size={24} />
-          </button>
+      <nav className="bg-gray-50 border-gray-200 rounded-b-3xl">
+        <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
+          <a className="" href="/">
+            <span className="text-2xl font-semibold hover:text-gray-400">TopicPost</span>
+          </a>
+          <div className="flex items-center md:order-2">
+            <button
+              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 focus:outline-none"
+              onClick={toggle}
+            >
+              Login
+            </button>
+            <button
+              data-collapse-toggle="mega-menu"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              aria-controls="mega-menu"
+              aria-expanded="false"
+              onClick={toggleDropdown}
+            >
+              <span className="sr-only">Open main menu</span>
+              <FiMenu size={24} />
+            </button>
+          </div>
+          <div id="mega-menu" className={`items-center justify-between ${isOpen ? '' : 'hidden'} w-full md:flex md:w-auto md:order-1`}>
+            <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
+              <li>
+                <a href="/" className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="/" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0">Team</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div id="mega-menu" className={`items-center justify-between ${isOpen ? '' : 'hidden'} w-full md:flex md:w-auto md:order-1`}>
-          <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-            <li>
-              <a href="/" className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="/" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0">Team</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
   )
 }
