4b7a15fb75da758187e9e7e9ccf2e6a261a3a302
chihiros <szrkw2ui2a@gmail.com>
2023-05-29 02:43:45 +0900

    fix: document format

--- src/apps/menu/pages/CompactMenu.tsx
+++ src/apps/menu/pages/CompactMenu.tsx
@@ -39,10 +39,12 @@ export const CompactMenu = () => {
           <div id="mega-menu" className={`items-center justify-between ${isOpen ? '' : 'hidden'} w-full md:flex md:w-auto md:order-1`}>
             <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
               <li>
-                <a href="/" className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="/" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0">Team</a>
+                <a
+                  href="/"
+                  className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0"
+                >
+                  Team
+                </a>
               </li>
             </ul>
           </div>
