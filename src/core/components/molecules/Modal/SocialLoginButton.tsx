593a68fa91d67240d3d9c942eb74a266dc44e643
chihiros <szrkw2ui2a@gmail.com>
2023-05-29 00:59:12 +0900

    fix: レイアウトのスタイルを変更

--- src/core/components/molecules/Modal/SocialLoginButton.tsx
+++ src/core/components/molecules/Modal/SocialLoginButton.tsx
@@ -10,7 +10,7 @@ export const SocialLoginButton: React.FC<SocialLoginProps> = ({ icon, children, 
   return (
     <button className="group w-full h-10 px-4 border-2 border-gray-300 rounded-full duration-300
                        hover:border-blue-400 active:bg-blue-100">
-      <div className="relative items-center space-x-4 justify-start grid grid-cols-4">
+      <div className="relative items-center space-x-4 justify-center grid grid-flow-col sm:grid-cols-4">
         <span className="ml-auto">{icon}</span>
         <span className="w-max font-semibold tracking-wide text-gray-700 duration-300 group-hover:text-blue-600">{children}</span>
       </div>
