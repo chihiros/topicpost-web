59b87d38d43dd0d53c2a61df459c8fa495520726
chihiros <szrkw2ui2a@gmail.com>
2023-05-29 00:51:35 +0900

    fix: cssスタイルの変更

--- src/core/components/molecules/Modal/SocialLoginButton.tsx
+++ src/core/components/molecules/Modal/SocialLoginButton.tsx
@@ -8,11 +8,11 @@ export interface SocialLoginProps {
 
 export const SocialLoginButton: React.FC<SocialLoginProps> = ({ icon, children, link }) => {
   return (
-    <button className="group w-full h-10 px-4 border-2 border-gray-300 rounded-full transition duration-300
+    <button className="group w-full h-10 px-4 border-2 border-gray-300 rounded-full duration-300
                        hover:border-blue-400 active:bg-blue-100">
       <div className="relative items-center space-x-4 justify-start grid grid-cols-4">
-        <span className="tile ml-auto">{icon}</span>
-        <span className="tile block w-max font-semibold tracking-wide text-gray-700 text-base transition duration-300 group-hover:text-blue-600">{children}</span>
+        <span className="ml-auto">{icon}</span>
+        <span className="w-max font-semibold tracking-wide text-gray-700 duration-300 group-hover:text-blue-600">{children}</span>
       </div>
     </button>
   );
