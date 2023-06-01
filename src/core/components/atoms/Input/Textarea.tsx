0e00177df7dc91f8216a0b40e2b6126861fceb4f
chihiros <szrkw2ui2a@gmail.com>
2023-06-02 02:36:33 +0900

    add: TextareaにDisableを追加

--- src/core/components/atoms/Input/Textarea.tsx
+++ src/core/components/atoms/Input/Textarea.tsx
@@ -7,13 +7,14 @@ type TextareaProps = {
   placeholder?: string;
   required?: boolean;
   value?: string;
+  disabled?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onDragOver?: (e: React.DragEvent<HTMLTextAreaElement>) => void;
   onDrop?: (e: React.DragEvent<HTMLTextAreaElement>) => void;
   onPaste?: (e: React.ClipboardEvent<HTMLTextAreaElement>) => void;
 };
 
-const Textarea: React.FC<TextareaProps> = ({ id, rows, className, placeholder, required, value, onChange, onDragOver, onDrop, onPaste }) => {
+const Textarea: React.FC<TextareaProps> = ({ id, rows, className, placeholder, required, value, disabled, onChange, onDragOver, onDrop, onPaste }) => {
   const [isFocused, setIsFocused] = React.useState(false);
 
   const handleFocus = () => {
@@ -40,6 +41,7 @@ const Textarea: React.FC<TextareaProps> = ({ id, rows, className, placeholder, r
       onDragOver={onDragOver}
       onDrop={onDrop}
       onPaste={onPaste}
+      {...(disabled ? { disabled } : {})}
     ></textarea>
   );
 }
