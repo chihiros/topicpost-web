d9b860547ec484afdadda54de4b98203fe162f58
chihiros <szrkw2ui2a@gmail.com>
2023-06-04 20:58:08 +0900

    fix: レクリエーションの情報をAPIから持ってくるように修正

--- src/apps/recreation/templates/RecreationContentTemplate.tsx
+++ src/apps/recreation/templates/RecreationContentTemplate.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import RecreationAPI, { RecreationResponse } from "../../../api/api.topicpost.net/recreation";
 
 export const RecreationContentTemplate: React.FC = () => {
   const prams = useParams<{ id: string }>();
   console.log(prams.id);
-
   const id = prams.id;
+
+  useEffect(() => {
+    const recreation = new RecreationAPI();
+    recreation.getByRecreationID(id).then((response: RecreationResponse) => {
+      console.log("response", response);
+    }).catch((error: any) => {
+      console.error(error);
+    });
+    console.log("useEffect", id);
+  }, []);
 
   return (
     <>
