e5edea13629c70f9dee1f331acb08aeafbe5e199
chihiros <szrkw2ui2a@gmail.com>
2023-06-04 19:25:55 +0900

    add: レクリエーションの詳細を表示してくれるページの追加

--- src/apps/recreation/templates/RecreationContentTemplate.tsx
+++ src/apps/recreation/templates/RecreationContentTemplate.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const RecreationContentTemplate: React.FC = () => {
+  const prams = useParams<{ id: string }>();
+  console.log(prams.id);
+
+  const id = prams.id;
+
   return (
-    <></>
+    <>
+      <div className='mb-2 ml-2 text-2xl'>
+        新着情報
+      </div>
+      <div className='flex justify-center'>
+        <div className='w-1/2'>
+          {id}
+        </div>
+      </div>
+    </>
   );
 }
