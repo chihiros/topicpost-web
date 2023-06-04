4c2263b23b5aed2469cd0b4350992f3a053e1001
chihiros <szrkw2ui2a@gmail.com>
2023-06-04 13:26:07 +0900

    fix: 不要なタグを削除

--- src/apps/recreation/organisms/RecreationCards.tsx
+++ src/apps/recreation/organisms/RecreationCards.tsx
@@ -27,45 +27,43 @@ export const RecreationCards: React.FC = () => {
 
 
   return (
-    <>
-      <div className="relative">
-        <div className="flex overflow-x-auto gap-4 mb-4 aaaaaaaaaaa">
-          {/* ここはAPIの通信で10個ほどの値を取得する */}
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-          <NewCard />
-        </div>
-        <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex justify-between">
-          <button className="bg-gray-200 hover:bg-gray-300 rounded-full w-12 h-12 opacity-60" onClick={
-            function () {
-              const element = document.querySelector('.aaaaaaaaaaa')
-              if (element) {
-                smoothScroll(element, -400, 500) // -200px左に移動して0.5秒かけて移動する
-              }
-            }
-          }>
-            ←
-          </button>
-          <button className="bg-gray-200 hover:bg-gray-300 rounded-full w-12 h-12 opacity-60" onClick={
-            function () {
-              const element = document.querySelector('.aaaaaaaaaaa')
-              if (element) {
-                smoothScroll(element, 400, 500) // +200px左に移動して0.5秒かけて移動する
-              }
-            }
-          }>
-            →
-          </button>
-        </div>
+    <div className="relative">
+      <div className="flex overflow-x-auto gap-4 mb-4 aaaaaaaaaaa">
+        {/* ここはAPIの通信で10個ほどの値を取得する */}
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
+        <NewCard />
       </div>
-    </>
+      <div className="absolute top-1/2 transform -translate-y-1/2 w-full flex justify-between">
+        <button className="bg-gray-200 hover:bg-gray-300 rounded-full w-12 h-12 opacity-60" onClick={
+          function () {
+            const element = document.querySelector('.aaaaaaaaaaa')
+            if (element) {
+              smoothScroll(element, -400, 500) // -200px左に移動して0.5秒かけて移動する
+            }
+          }
+        }>
+          ←
+        </button>
+        <button className="bg-gray-200 hover:bg-gray-300 rounded-full w-12 h-12 opacity-60" onClick={
+          function () {
+            const element = document.querySelector('.aaaaaaaaaaa')
+            if (element) {
+              smoothScroll(element, 400, 500) // +200px左に移動して0.5秒かけて移動する
+            }
+          }
+        }>
+          →
+        </button>
+      </div>
+    </div>
   );
 }
