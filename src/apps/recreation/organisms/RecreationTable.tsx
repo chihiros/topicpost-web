60b3c8b2f5b9b75ff26f7326ab341dcd2b2f9012
chihiros <szrkw2ui2a@gmail.com>
2023-06-04 17:56:27 +0900

    fix: react-iconsを使うように修正

--- src/apps/recreation/organisms/RecreationTable.tsx
+++ src/apps/recreation/organisms/RecreationTable.tsx
@@ -3,6 +3,7 @@ import { AiOutlineSearch, AiOutlinePlus } from "react-icons/ai";
 import { BsFilter } from "react-icons/bs";
 import { useHistory } from 'react-router-dom';
 import Recreation, { RecreationsResponse } from '../../../api/api.topicpost.net/recreation';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export const RecreationTable: React.FC = () => {
   const [isFilterDropdownOpen, setFilterDropdownOpen] = useState(false);
@@ -206,9 +207,7 @@ export const RecreationTable: React.FC = () => {
               onClick={handlePageCalc(-1)}
               className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
               <span className="sr-only">Previous</span>
-              <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
-              </svg>
+              <IoIosArrowBack size={16} />
             </a>
           </li>
           {pageNumbers.map(number => (
@@ -228,9 +227,7 @@ export const RecreationTable: React.FC = () => {
               onClick={handlePageCalc(1)}
               className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
               <span className="sr-only">Next</span>
-              <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-              </svg>
+              <IoIosArrowForward size={16} />
             </a>
           </li>
         </ul>
