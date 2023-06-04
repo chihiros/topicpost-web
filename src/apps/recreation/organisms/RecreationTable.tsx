f638220ada3f66b7ea33cc7b488deadba844a16c
chihiros <szrkw2ui2a@gmail.com>
2023-06-04 19:13:40 +0900

    fix: tableのレコードを選択した詳細に飛ぶように変更

--- src/apps/recreation/organisms/RecreationTable.tsx
+++ src/apps/recreation/organisms/RecreationTable.tsx
@@ -177,21 +177,20 @@ export const RecreationTable: React.FC = () => {
           </thead>
           <tbody>
             {recreations?.data.recreations.map((Recreation, key) => (
-              <tr
-                key={key}
-                className="border-b hover:bg-gray-100"
-              >
-                <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">{Recreation.title}</th>
-                <td className="px-4 py-3">
-                  {Recreation.genre.map((genre, key) => (
-                    <pre key={key} className={`bg-gray-200 text-blue-600 text-xs text-center px-1 inline ${key === Recreation.genre.length - 1 ? '' : 'mb-1 mr-1'} rounded`}>
-                      {GetRecreationGenre(genre)}
-                    </pre>
-                  ))}
-                </td>
-                <td className="px-4 py-3 hidden sm:table-cell">すずりかわ@熊本</td>
-                <td className="px-4 py-3 hidden xl:table-cell">{Recreation.created_at}</td>
-              </tr>
+              <Link to={`/recreation/${Recreation.recreation_id}`} key={key}>
+                <tr className="border-b hover:bg-gray-100">
+                  <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">{Recreation.title}</th>
+                  <td className="px-4 py-3">
+                    {Recreation.genre.map((genre, key) => (
+                      <pre key={key} className={`bg-gray-200 text-blue-600 text-xs text-center px-1 inline ${key === Recreation.genre.length - 1 ? '' : 'mb-1 mr-1'} rounded`}>
+                        {GetRecreationGenre(genre)}
+                      </pre>
+                    ))}
+                  </td>
+                  <td className="px-4 py-3 hidden sm:table-cell">すずりかわ@熊本</td>
+                  <td className="px-4 py-3 hidden xl:table-cell">{Recreation.created_at}</td>
+                </tr>
+              </Link>
             ))}
           </tbody>
         </table>
