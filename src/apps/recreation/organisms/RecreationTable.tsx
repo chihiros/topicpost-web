93728585daeb6bf0a8837a970e55bed92b0a573d
chihiros <szrkw2ui2a@gmail.com>
2023-06-04 17:47:08 +0900

    fix: handlerの追加

--- src/apps/recreation/organisms/RecreationTable.tsx
+++ src/apps/recreation/organisms/RecreationTable.tsx
@@ -29,6 +29,19 @@ export const RecreationTable: React.FC = () => {
   const [recreation_records, setRecreationRecords] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const handlePageClick = (pageNumber: number) => (e: React.MouseEvent) => {
+    e.preventDefault();
+    setCurrentPage(pageNumber);
+  }
+
+  const handlePageCalc = (n: number) => (e: React.MouseEvent) => {
+    if (currentPage + n < 1) {
+      return;
+    }
+
+    setCurrentPage(currentPage + n);
+  }
+
   useEffect(() => {
     const recreation = new Recreation();
     recreation.get(currentPage).then((response: RecreationsResponse) => {
@@ -184,7 +197,10 @@ export const RecreationTable: React.FC = () => {
         </span>
         <ul className="inline-flex items-stretch -space-x-px">
           <li>
-            <a href="/" className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+            <a
+              href="/"
+              onClick={handlePageCalc(-1)}
+              className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
               <span className="sr-only">Previous</span>
               <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -192,19 +208,22 @@ export const RecreationTable: React.FC = () => {
             </a>
           </li>
           <li>
-            <a href="/" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
+            <a href="/" onClick={handlePageClick(1)} className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
           </li>
           <li>
-            <a href="/" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
+            <a href="/" onClick={handlePageClick(2)} className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
           </li>
           <li>
-            <a href="/" className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700">3</a>
+            <a href="/"　onClick={handlePageClick(3)} className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700">3</a>
           </li>
           <li>
             <a href="/" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">100</a>
           </li>
           <li>
-            <a href="/" className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+            <a
+              href="/"
+              onClick={handlePageCalc(1)}
+              className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
               <span className="sr-only">Next</span>
               <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
