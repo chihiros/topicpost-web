de7b8f725fea8263bc802f8b0e583cc97de20af4
chihiros <szrkw2ui2a@gmail.com>
2023-06-06 17:06:45 +0900

    fix: Templateを使った書き方に変更

--- src/apps/recreation/pages/RecreationTopPage.tsx
+++ src/apps/recreation/pages/RecreationTopPage.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { RecreationCards } from '../organisms/RecreationCards';
-import { RecreationTable } from '../organisms/RecreationTable';
+import { RecreationTopTemplate } from '../templates/RecreationTopTemplate';
 
 export const RecreationTopPage: React.FC = () => {
   return (
-    <>
-      <div className='mb-2 ml-2 text-2xl'>
-        新着情報
-      </div>
-      <RecreationCards />
-
-      <div className='mt-6 mb-2 ml-2 text-2xl flex justify-between'>
-        一覧
-      </div>
-      <RecreationTable />
-    </>
+    <RecreationTopTemplate />
   );
 }
