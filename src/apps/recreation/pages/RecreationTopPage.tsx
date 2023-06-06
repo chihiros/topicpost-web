aad26960a724fbba5bb37acea92d825d69ac15b9
chihiros <szrkw2ui2a@gmail.com>
2023-06-06 15:01:42 +0900

    del: 不要な改行の削除

--- src/apps/recreation/pages/RecreationTopPage.tsx
+++ src/apps/recreation/pages/RecreationTopPage.tsx
@@ -3,10 +3,6 @@ import { RecreationCards } from '../organisms/RecreationCards';
 import { RecreationTable } from '../organisms/RecreationTable';
 
 const RecreationForm: React.FC = () => {
-      </div>
-      <RecreationCards />
-
-      <div className='mt-6 mb-2 ml-2 text-2xl flex justify-between'>
         一覧
       </div>
       <RecreationTable />
