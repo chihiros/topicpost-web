006f3a022f77022f095b140a64782ca6cff29a21
chihiros <szrkw2ui2a@gmail.com>
2023-06-06 15:02:03 +0900

    revert: 修正分を戻す

--- src/apps/recreation/pages/RecreationTopPage.tsx
+++ src/apps/recreation/pages/RecreationTopPage.tsx
@@ -3,6 +3,14 @@ import { RecreationCards } from '../organisms/RecreationCards';
 import { RecreationTable } from '../organisms/RecreationTable';
 
 const RecreationForm: React.FC = () => {
+  return (
+    <>
+      <div className='mb-2 ml-2 text-2xl'>
+        新着情報
+      </div>
+      <RecreationCards />
+
+      <div className='mt-6 mb-2 ml-2 text-2xl flex justify-between'>
         一覧
       </div>
       <RecreationTable />
