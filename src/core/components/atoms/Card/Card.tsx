90151472b6c3f9dc5552d59d0dd0d9bf972b5bef
chihiros <szrkw2ui2a@gmail.com>
2023-06-11 21:20:19 +0900

    fix: document format

--- src/core/components/atoms/Card/Card.tsx
+++ src/core/components/atoms/Card/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Twemoji  from 'react-twemoji';
+import Twemoji from 'react-twemoji';
 
 type CardProps = {
   title: string;
