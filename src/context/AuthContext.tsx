eb64a0813b113fbffc822113c1f6f7d1fbcfe4f7
chihiros <szrkw2ui2a@gmail.com>
2023-06-11 18:59:47 +0900

    fix: Authの中でProfileのデータを管理するように修正

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,20 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import { supabaseClient } from '../utils/supabase';
+import ProfileAPI, { ProfileData, ProfileResponse } from '../api/api.topicpost.net/profile';
+import { useProfileDataContext } from './ProfileDataContext';
 
 interface AuthContextType {
   isLoggedIn: boolean;
   setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoggedInTrue: () => void;
+  setLoggedInFalse: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType>({
   isLoggedIn: false,
   setLoggedIn: () => { },
+  setLoggedInTrue: () => { },
+  setLoggedInFalse: () => { },
 });
 
 export const useAuthContext = () => {
@@ -21,6 +27,17 @@ interface Props {
 
 export const AuthContextProvider: React.FC<Props> = ({ children }) => {
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const { profileData, setProfileData, getProfileData, isProfileDataUndefined } = useProfileDataContext();
+
+  const setLoggedInTrue = () => {
+    setLoggedIn(true);
+    getProfileData();
+  };
+
+  const setLoggedInFalse = () => {
+    setLoggedIn(false);
+    setProfileData(undefined);
+  };
 
   useEffect(() => {
     const checkSession = async () => {
@@ -31,15 +48,21 @@ export const AuthContextProvider: React.FC<Props> = ({ children }) => {
 
     supabaseClient.auth.onAuthStateChange((event) => {
       if (event === 'SIGNED_IN' || event === 'USER_UPDATED') {
-        setLoggedIn(true);
+        setLoggedInTrue();
       } else if (event === 'SIGNED_OUT') {
-        setLoggedIn(false);
+        setLoggedInFalse();
+        setProfileData(undefined);
       }
     });
-  }, []);
+    if (isProfileDataUndefined) { // プロフォールが取得されていない場合
+      if (!isLoggedIn) { // ログインしていない場合
+        getProfileData();
+      }
+    }
+  }, [isLoggedIn, profileData, isProfileDataUndefined]);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn, setLoggedInTrue, setLoggedInFalse }}>
       {children}
     </AuthContext.Provider>
   );
