b52a6f7b001e464f59d4a6076ed646c289d1b789
chihiros <szrkw2ui2a@gmail.com>
2023-06-16 05:16:06 +0900

    add: お知らせの項目を追加

--- src/apps/common/TopScreen.tsx
+++ src/apps/common/TopScreen.tsx
@@ -17,6 +17,28 @@ const Top: React.FC = () => {
   return (
     <>
       <div className='mb-2 ml-2 text-2xl'>
+        お知らせ
+      </div>
+      <div className="p-4 bg-gray-50 rounded-lg mb-6">
+        <div className="flex">
+          <div className="flex-shrink-0">
+            <svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                d="M13 10V3L4 14h7v7l9-11h-7z" />
+            </svg>
+          </div>
+          <div className="ml-3">
+            <p className="text-base font-medium text-gray-900">
+              お知らせのタイトル
+            </p>
+            <p className="mt-1 text-sm text-gray-500">
+              お知らせの内容
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='mb-2 ml-2 text-2xl'>
         レクリエーション
       </div>
       <RecreationCards
