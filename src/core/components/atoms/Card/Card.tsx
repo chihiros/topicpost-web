7cf8afdd375a5d895cccf7cb167c8b266fcad33f
chihiros <szrkw2ui2a@gmail.com>
2023-06-16 03:17:42 +0900

    fix: 投稿者のアイコンと、名前を表示するように修正

--- src/core/components/atoms/Card/Card.tsx
+++ src/core/components/atoms/Card/Card.tsx
@@ -72,9 +72,14 @@ const Card: React.FC<CardProps> = ({ title, date, content, recreationId, data })
         <div className="p-5">
           <div className="mb-2 text-base font-bold tracking-tight text-gray-900">{data.title}</div>
           <p className="text-gray-700 text-left text-sm mb-1">{formatDate(data.created_at)}</p>
-          {/* <MarkdownPreview> */}
-          {previewContent(data.content)}
-          {/* </MarkdownPreview> */}
+          <div className='flex items-center justify-end'>
+            <img
+              src={data.edges.profile.icon_url}
+              className='w-5 h-5 rounded-full inline'
+              alt='ユーザーアイコン'
+            />
+            <span className='text-sm text-slate-800 pl-2'>{data.edges.profile.nickname}</span>
+          </div>
         </div>
       </Link>
     </div>
