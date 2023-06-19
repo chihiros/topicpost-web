7509b51eaa794a5cf3465e8dc934afd581b1fa24
chihiros <szrkw2ui2a@gmail.com>
2023-06-19 23:12:17 +0900

    revert: 変更を戻す

--- src/core/components/atoms/Markdown/MarkdownPreview.tsx
+++ src/core/components/atoms/Markdown/MarkdownPreview.tsx
@@ -30,38 +30,38 @@ export const MarkdownPreview: React.FC<Props> = ({ children }) => {
             `}
       </style>
       <ReactMarkdown
-        className='prose-md'
+        className='prose'
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw]}
         // rehypePlugins={[rehypeRaw, [rehypeSanitize]]}
         components={{
           h1: ({ node, children, ...props }) => (
-            <div className="font-bold mb-2 text-3xl" {...props}>
+            <h1 className="font-bold mb-2" {...props}>
               {children || ''}
-            </div>
+            </h1>
           ),
           h2: ({ node, children, ...props }) => (
-            <div className="m-0 font-bold mb-2 text-2xl" {...props}>
+            <h2 className="m-0 font-bold mb-2" {...props}>
               {children || ''}
-            </div>
+            </h2>
           ),
           h3: ({ node, children, ...props }) => (
-            <div className="m-0 font-bold mb-2 text-xl" {...props}>
+            <h3 className="m-0 font-bold mb-2" {...props}>
               {children || ''}
-            </div>
+            </h3>
           ),
           h4: ({ node, children, ...props }) => (
-            <h4 className="m-0 font-bold mb-2 text-lg" {...props}>
+            <h4 className="m-0 font-bold mb-2" {...props}>
               {children || ''}
             </h4>
           ),
           h5: ({ node, children, ...props }) => (
-            <h5 className="m-0 mb-2 text-base" {...props}>
+            <h5 className="m-0 mb-2" {...props}>
               {children || ''}
             </h5>
           ),
           h6: ({ node, children, ...props }) => (
-            <h6 className="m-0 mb-2 text-sm" {...props}>
+            <h6 className="m-0 mb-2" {...props}>
               {children || ''}
             </h6>
           ),
@@ -96,12 +96,13 @@ export const MarkdownPreview: React.FC<Props> = ({ children }) => {
             );
           },
           code: ({ node, inline, className, children, ...props }) => {
+            // const match = /language-(\w+)/.exec(className || '')
             return inline ? (
               <code className="bg-gray-200 text-blue-600 px-1 rounded" {...props}>
                 {children}
               </code>
             ) : (
-              <pre className="bg-gray-800 text-gray-200 p-3 text-sm m-0 rounded" {...props}>
+              <pre className="bg-gray-800 text-gray-200 p-0 m-0 rounded" {...props}>
                 {children}
               </pre>
             )
